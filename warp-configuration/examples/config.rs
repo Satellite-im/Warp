384a8f6fc3ffe141605e137c0b198e1961a0147d
Darius Clark <darius.clark93@gmail.com>
2022-02-04 02:37:32 -0500

    feat(config): Updated warp-configuration crate
    Implemented functions to use for `Config`. This would allow one to handle loading and saving the configuration or to a reader and writer.
    
    Renamed `APIConfig` to `HTTPAPIConfig`
    
    Added `Textile` and `WebTorrent` to `FileSystem` and have all enum serialize to lowercase.

--- warp-configuration/examples/config.rs
+++ warp-configuration/examples/config.rs
@@ -1,6 +1,22 @@
+use warp_configuration::Config;
 
 
-fn main() {
-    let cfg: warp_configuration::Config = warp_configuration::get().unwrap();
+fn enable_debug(config: &mut Config) -> Result<(), warp_configuration::error::Error> {
+    config.debug = true;
+    config.save("Warp.test.toml")
+}
 
+fn disable_debug(config: &mut Config) -> Result<(), warp_configuration::error::Error> {
+    config.debug = false;
+    config.save("Warp.test.toml")
+}
+
+fn main() -> Result<(), warp_configuration::error::Error> {
+    let mut config = Config::new();
+    config.save("Warp.test.toml")?;
+    assert_eq!(config.debug, true);
+    disable_debug(&mut config)?;
+    assert_eq!(config.debug, false);
+    std::fs::remove_file("Warp.test.toml")?;
+    Ok(())
 }
--- warp-configuration/src/lib.rs
+++ warp-configuration/src/lib.rs
@@ -1,48 +1,137 @@
 pub mod error;
 
-use serde::Deserialize; // https://docs.serde.rs/serde/
+use std::io::{Read, Write};
+use std::path::Path;
+use serde::{Serialize, Deserialize}; // https://docs.serde.rs/serde/
 use crate::error::Error;
 
 // Acceptable module implementations for the FileSystem
-#[derive(Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="lowercase")] // https://serde.rs/container-attrs.html
 pub enum FileSystem {
-    DISK,
+    Disk,
+    Textile,
+    WebTorrent
 }
 
 // Acceptable module implementations for the Cache
-#[derive(Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all="lowercase")]
 pub enum PocketDimension {
-    FLATFILE,
+    FlatFile,
 }
 
 /// Represents options related to the REST API
-#[derive(Deserialize)]
-pub struct APIConfig {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HTTPAPIConfig {
     pub enabled: bool,
 }
 
 /// Defines which implementations to load for each module
-#[derive(Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ModuleConfig {
     pub pocket_dimension: PocketDimension,
     pub file_system: FileSystem,
 }
 /// Represents the global config for Warp
-#[derive(Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub debug: bool,
-    pub api: APIConfig,
+    pub http_api: HTTPAPIConfig,
     pub modules: ModuleConfig
 }
 
-/// Returns the parsed TOML config file for Warp
-/// # Examples
-///
-/// ```
-/// let cfg = warp_configuration::get().unwrap();
-/// ```
-pub fn get() -> Result<Config, Error> {
-    let local_config: String = std::fs::read_to_string("./Warp.toml")?;
-    let config: Config = toml::from_str(&local_config)?;
-    return Ok(config);
+// Implementation to create, load and save the config
+impl Config {
+    /// Creates the configuration
+    /// # Examples
+    ///
+    /// ```
+    /// use warp_configuration::Config;
+    /// let config = Config::new();
+    /// ```
+    pub fn new() -> Config {
+        Config {
+            debug: true,
+            http_api: HTTPAPIConfig {
+                enabled: false
+            },
+            modules: ModuleConfig {
+                pocket_dimension: PocketDimension::FlatFile,
+                file_system: FileSystem::Disk
+            }
+        }
+    }
+
+    /// Loads and return the parsed TOML configuration file for Warp
+    /// # Examples
+    ///
+    /// ```
+    /// use warp_configuration::Config;
+    /// let config = Config::load("Warp.toml").unwrap();
+    /// ```
+    pub fn load<P: AsRef<Path>>(
+        path: P
+    ) -> Result<Config, Error> {
+        let path = path.as_ref();
+        if !path.exists() {
+            return Err(Error::ConfigNotFound);
+        }
+        let config_data = std::fs::read_to_string(path)?;
+        toml::from_str(&config_data).map_err(Error::from)
+    }
+
+    /// Loads and return the parsed TOML configuration from reader
+    /// # Examples
+    ///
+    /// ```
+    /// use warp_configuration::Config;
+    /// use std::fs::File;
+    ///
+    /// let mut file = File::open("Warp.toml").unwrap();
+    ///
+    /// let config = Config::from_reader(&mut file).unwrap();
+    /// ```
+    pub fn from_reader<R: Read>(
+        reader: &mut R
+    ) -> Result<Config, Error> {
+        let mut data = String::new();
+        reader.read_to_string(&mut data)?;
+        toml::from_str(&data).map_err(Error::from)
+    }
+
+    /// Saves the configuration to disk
+    /// # Examples
+    ///
+    /// ```
+    /// use warp_configuration::Config;
+    /// let config = Config::new();
+    /// config.save("Warp.toml").unwrap();
+    /// ```
+    pub fn save<P: AsRef<Path>>(
+        &self,
+        path: P
+    ) -> Result<(), Error> {
+        let config_data = toml::to_string(&self)?;
+        std::fs::write(path, config_data)?;
+        Ok(())
+    }
+
+    /// Saves the configuration to writer
+    /// # Examples
+    ///
+    /// ```
+    /// use warp_configuration::Config;
+    /// use std::fs::File;
+    ///
+    /// let mut file = File::create("Warp.toml").unwrap();
+    /// let config = Config::new();
+    /// config.save_to_writer(&mut file).unwrap();
+    /// ```
+    pub fn save_to_writer<W: Write>(&self, writer: &mut W) -> Result<(), Error> {
+        let config_data = toml::to_string(&self)?;
+        writer.write_all(config_data.as_bytes())?;
+        writer.flush()?;
+        Ok(())
+    }
 }
