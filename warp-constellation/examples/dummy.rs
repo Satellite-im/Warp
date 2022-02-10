5db5a4771d02ab1437131a5f8f9e8727f3b6522c
Darius Clark <darius.clark93@gmail.com>
2022-02-10 05:28:07 -0500

    chore(fs): Move `DummyFileSystem` into examples
    chore(fs): Move `DummyFileSystem` into examples

--- warp-constellation/examples/dummy.rs
+++ warp-constellation/examples/dummy.rs
@@ -1,7 +1,53 @@
-use warp_constellation::constellation::{Constellation, dummy::DummyFileSystem};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use warp_constellation::constellation::Constellation;
 use warp_constellation::directory::{Directory, DirectoryType};
 use warp_constellation::file::File;
 use warp_constellation::item::Item;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct DummyFileSystem {
+    version: ConstellationVersion,
+    #[serde(skip_serializing)]
+    root_directory: Directory,
+    index: Directory,
+    modified: DateTime<Utc>
+}
+
+impl Default for DummyFileSystem {
+    fn default() -> Self {
+        DummyFileSystem {
+            version: ConstellationVersion::from((0, 1, 2)),
+            root_directory: Directory::new("root", DirectoryType::Default),
+            index: Directory::new("root", DirectoryType::Default),
+            modified: Utc::now()
+        }
+    }
+}
+
+impl Constellation for DummyFileSystem {
+
+    fn version(&self) -> &ConstellationVersion {
+        &self.version
+    }
+
+    fn modified(&self) -> DateTime<Utc> {
+        self.modified
+    }
+
+    fn root_directory(&self) -> &Directory {
+        &self.index
+    }
+
+    fn current_directory(&self) -> &Directory {
+        &self.root_directory
+    }
+
+    fn current_directory_mut(&mut self) -> &mut Directory {
+        &mut self.root_directory
+    }
+
+}
 
 fn main() -> Result<(), warp_constellation::error::Error> {
     let mut dummy_fs = DummyFileSystem::default();
