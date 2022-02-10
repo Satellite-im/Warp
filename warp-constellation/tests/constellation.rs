5f0f17af7c916ce71aac3d3b8b539d82937bf2b9
Darius Clark <darius.clark93@gmail.com>
2022-02-10 13:51:38 -0500

    fix(fs): Corrected constellation test

--- warp-constellation/tests/constellation.rs
+++ warp-constellation/tests/constellation.rs
@@ -1,11 +1,58 @@
 
+
+
 #[cfg(test)]
 mod tests {
   use warp_constellation::directory::{Directory, DirectoryType};
   use warp_constellation::file::File;
-  use warp_constellation::constellation::{Constellation, dummy::DummyFileSystem};
+  use warp_constellation::constellation::{Constellation, ConstellationVersion};
   use warp_constellation::item::Item;
+  use chrono::{DateTime, Utc};
+  use serde::{Deserialize, Serialize};
 
+  #[derive(Serialize, Deserialize, Clone, Debug)]
+  pub struct DummyFileSystem {
+    version: ConstellationVersion,
+    #[serde(skip_serializing)]
+    root_directory: Directory,
+    index: Directory,
+    modified: DateTime<Utc>
+  }
+
+  impl Default for DummyFileSystem {
+    fn default() -> Self {
+      DummyFileSystem {
+        version: ConstellationVersion::from((0, 1, 2)),
+        root_directory: Directory::new("root", DirectoryType::Default),
+        index: Directory::new("root", DirectoryType::Default),
+        modified: Utc::now()
+      }
+    }
+  }
+
+  impl Constellation for DummyFileSystem {
+
+    fn version(&self) -> &ConstellationVersion {
+      &self.version
+    }
+
+    fn modified(&self) -> DateTime<Utc> {
+      self.modified
+    }
+
+    fn root_directory(&self) -> &Directory {
+      &self.index
+    }
+
+    fn current_directory(&self) -> &Directory {
+      &self.root_directory
+    }
+
+    fn current_directory_mut(&mut self) -> &mut Directory {
+      &mut self.root_directory
+    }
+
+  }
 
   #[test]
   fn loads() -> Result<(), warp_constellation::error::Error>{
