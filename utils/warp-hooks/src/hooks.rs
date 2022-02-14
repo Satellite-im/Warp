295a5b29748fb7717eeb158c56956de2ac7c8877
RetroPronghorn <retropronghorn@gmail.com>
2022-02-14 00:08:19 -0500

    chore(Restructure): Restructure the application

--- utils/warp-hooks/src/hooks.rs
+++ utils/warp-hooks/src/hooks.rs
@@ -10,7 +10,7 @@ pub struct DataObject {
     pub timestamp: DateTime<Utc>,
     pub size: u64,
     pub module: Module,
-    pub payload: ()
+    pub payload: (),
 }
 
 pub struct Hook {
@@ -18,10 +18,14 @@ pub struct Hook {
   data: DataObject,
 }
 
-/// Interface that would provide functionality around the filesystem.
+/// Interface that would provide functionality to the hook utility.
 pub trait Hooks {
   /// Returns the supported hooks
   fn hooks(&self) -> Vec<String>;
   // Registers a new hook for binding to
-  fn new(&self, name: String, module: Module) -> Result<(), Error>;
+  fn create(&self, name: &str, module: Module) -> Result<(), Error>;
+  // Trigger a hook given the supplied data object (which will be given to all subscribers on emission)
+  fn trigger(&self, name: &str, data: DataObject) -> Result<(), Error>;
+  // Subscribe to a new hook, the provided function (Subscriber) will be called when the hook is triggered
+  fn subscribe<Subscriber: Fn(Hook)>(&mut self, name: &str, f: Subscriber) -> Result<(), Error>;
 }
