2e9e20c6e9c8a6724c612296c826903bcc362aae
Darius Clark <darius.clark93@gmail.com>
2022-02-14 22:41:04 -0500

    chore(constellation): Updated test related to constellation to add more functionality.

--- modules/warp-constellation/tests/constellation.rs
+++ modules/warp-constellation/tests/constellation.rs
@@ -3,8 +3,7 @@
 
 #[cfg(test)]
 mod tests {
-  use warp_constellation::directory::{Directory};
-  use warp_constellation::file::File;
+  use warp_constellation::directory::{Directory, DirectoryType};
   use warp_constellation::constellation::{Constellation, ConstellationVersion};
   use chrono::{DateTime, Utc};
   use serde::{Deserialize, Serialize};
@@ -12,8 +11,6 @@ mod tests {
   #[derive(Serialize, Deserialize, Clone, Debug)]
   pub struct DummyFileSystem {
     version: ConstellationVersion,
-    #[serde(skip_serializing)]
-    root_directory: Directory,
     index: Directory,
     modified: DateTime<Utc>
   }
@@ -22,7 +19,6 @@ mod tests {
     fn default() -> Self {
       DummyFileSystem {
         version: ConstellationVersion::from((0, 1, 2)),
-        root_directory: Directory::new("root"),
         index: Directory::new("root"),
         modified: Utc::now()
       }
@@ -47,33 +43,33 @@ mod tests {
       &mut self.index
     }
 
-    fn current_directory(&self) -> &Directory {
-      &self.root_directory
-    }
-
-    fn current_directory_mut(&mut self) -> &mut Directory {
-      &mut self.root_directory
-    }
-
   }
 
   #[test]
-  fn loads() -> Result<(), warp_constellation::error::Error>{
+  fn test() -> Result<(), warp_constellation::error::Error>{
     let mut filesystem = DummyFileSystem::default();
 
-    let file0 = File::new("testFile.png");
-    let file1 = File::new("testPng2.png");
-    let file2 = File::new("abc.png");
-    let file3 = File::new("cc123.png");
+    filesystem.create_file("testFile.png")?;
+    filesystem.create_file("testPng2.png")?;
+    filesystem.create_file("abc.png")?;
+    filesystem.create_file("cc123.png")?;
+    filesystem.create_directory("Test Directory", DirectoryType::Default)?;
 
-    let mut directory = Directory::new("Test Directory");
-    directory.add_child(file0)?;
-    directory.add_child(file1)?;
-    directory.add_child(file2)?;
-    directory.add_child(file3)?;
+    assert_eq!(filesystem.has_child("testFile.png"), true);
+    assert_eq!(filesystem.has_child("testPng2.png"), true);
+    assert_eq!(filesystem.has_child("abc.png"), true);
+    assert_eq!(filesystem.has_child("cc123.png"), true);
 
-    filesystem.add_child(directory)?;
+    let root = filesystem.open_directory("")?;
 
+    root.rename_child("abc.png", "test.png")?;
+
+    assert_eq!(root.has_child("abc.png"), false);
+
+    root.move_item_to("testFile.png", "Test Directory")?;
+
+    assert_eq!(root.has_child("testFile.png"), false);
+    assert_eq!(root.get_child_by_path("Test Directory/testFile.png").is_ok(), true);
     //TODO: Complete test
     Ok(())
   }
