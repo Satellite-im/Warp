0162a1e3473d9bfc8417c394e0bf8a74d09f5f9a
Darius Clark <darius.clark93@gmail.com>
2022-02-16 12:24:26 -0500

    feat(pocket-dimension): Implemented `PocketDimension` and `QueryBuilder`
    Note: `QueryBuilder` may undergo another change.

--- modules/warp-pocket-dimension/src/error.rs
+++ modules/warp-pocket-dimension/src/error.rs
@@ -2,10 +2,14 @@ use thiserror::Error;
 
 
 
-#[derive(Error, Debug, PartialEq)]
+#[derive(Error, Debug)]
 pub enum Error {
     #[error("Regex Error: {0}")]
     RegexError(#[from] regex::Error),
+    #[error("{0}")]
+    SerdeJsonError(#[from] serde_json::Error),
+    #[error("{0}")]
+    WarpDataError(#[from] warp_data::error::Error),
     #[error("Unknown error has occurred")]
     Other
 }
