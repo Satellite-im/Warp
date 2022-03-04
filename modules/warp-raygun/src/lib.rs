85c49d57b579ad7211256c117fbe5bbabb53158f
Darius Clark <darius.clark93@gmail.com>
2022-03-04 15:01:39 -0500

    add(raygun): Initial implementation of `warp-raygun` interface

--- modules/warp-raygun/src/lib.rs
+++ modules/warp-raygun/src/lib.rs
@@ -1,8 +1,95 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use warp_common::chrono::{DateTime, Utc};
+use warp_common::serde::{Deserialize, Serialize};
+use warp_common::uuid::Uuid;
+use warp_common::Result;
+
+pub type Callback = Box<dyn Fn()>;
+
+#[derive(Clone, PartialEq, Eq)]
+pub struct MessageOptions<'a> {
+    pub smart: Option<bool>,
+    pub date_range: Option<&'a [DateTime<Utc>; 2]>,
+    pub id_range: Option<&'a [Uuid; 2]>,
+    pub limit: Option<i64>,
+    pub skip: Option<i64>,
+}
+
+#[derive(Clone, Deserialize, Serialize, Debug, PartialEq, Eq)]
+#[serde(crate = "warp_common::serde")]
+pub struct Message {
+    pub id: Uuid,
+    pub conversation_id: Uuid,
+    pub sender: Uuid,
+    pub date: DateTime<Utc>,
+    pub pinned: bool,
+    pub reactions: Vec<Reaction>,
+    pub value: Vec<String>,
+}
+
+#[derive(Clone, Deserialize, Serialize, Debug, PartialEq, Eq)]
+#[serde(crate = "warp_common::serde")]
+pub struct Reaction {
+    pub emoji: String,
+    pub users: Vec<Uuid>,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+pub enum ReactionState {
+    Add,
+    Remove,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+pub enum PinState {
+    Pin,
+    Unpin,
+}
+
+#[derive(Clone, PartialEq, Eq)]
+pub enum EmbedState {
+    Enabled,
+    Disable,
+}
+
+pub trait RayGun {
+    /// Retreive all messages
+    fn get_messages(
+        &self,
+        conversation_id: Uuid,
+        options: MessageOptions,
+        callback: Option<Callback>,
+    ) -> Result<Vec<Message>>;
+
+    fn send_message(
+        &mut self,
+        conversation_id: Uuid,
+        message_id: Option<Uuid>,
+        message: Vec<String>,
+    ) -> Result<()>;
+
+    fn delete_message(&mut self, conversation_id: Uuid, message_id: Uuid) -> Result<()>;
+
+    fn react(
+        &mut self,
+        conversation_id: Uuid,
+        message_id: Uuid,
+        state: ReactionState,
+        emoji: Option<String>,
+    ) -> Result<()>;
+
+    fn pin_message(
+        &mut self,
+        conversation_id: Uuid,
+        message_id: Uuid,
+        state: PinState,
+    ) -> Result<()>;
+
+    fn reply(
+        &mut self,
+        conversation_id: Uuid,
+        message_id: Uuid,
+        message: Vec<String>,
+    ) -> Result<()>;
+
+    fn embeds(&mut self, conversation_id: Uuid, message_id: Uuid, state: EmbedState) -> Result<()>;
 }
