0b9a0f4e70ae2600c707fdd67a640324a4b0ba8f
Darius Clark <darius.clark93@gmail.com>
2022-03-10 22:23:11 -0500

    chore(warp): Added constellation export with a placeholder.

--- warp/src/http/fs/mod.rs
+++
@@ -1,9 +0,0 @@
-use warp_common::anyhow;
-
-#[allow(unused_imports)]
-use rocket::{self, Rocket, Request, Build, routes, catchers, get, response::{content, status}, http::Status};
-
-#[get("/fs/export")]
-fn index() -> String {
-    format!("Yep")
-}
