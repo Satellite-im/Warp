acec8b990d6b7ad0cdd1232ea9530b2dc6bedd3d
Darius Clark <darius.clark93@gmail.com>
2022-03-12 00:41:04 -0500

    add(fs): Implemented uploading and downloading for `warp-fs-storj`
    TODO: Implement cache when dealing with uploading and downloading.

--- extensions/warp-fs-storj/src/lib.rs
+++ extensions/warp-fs-storj/src/lib.rs
@@ -1,27 +1,21 @@
-
-
 use std::sync::{Arc, Mutex};
 
-
+use warp_common::{
+    anyhow,
+    chrono::{DateTime, Utc},
+    error::Error,
+    serde::{Deserialize, Serialize},
+    tokio, Extension,
+};
+use warp_constellation::{constellation::Constellation, directory::Directory};
 use warp_pocket_dimension::PocketDimension;
-use warp_common::chrono::{DateTime, Utc};
-use warp_common::error::Error;
-use warp_common::anyhow;
-use warp_common::serde::{Deserialize, Serialize};
-use warp_common::Extension;
-use warp_constellation::constellation::{Constellation, ConstellationVersion};
-use warp_constellation::directory::Directory;
-use warp_constellation::file::File;
-use warp_data::DataObject;
-use warp_module::Module;
-use warp_pocket_dimension::query::QueryBuilder;
-use warp_common::tokio;
 
-
-use s3::BucketConfiguration;
 use s3::bucket::Bucket;
 use s3::creds::Credentials;
 use s3::region::Region;
+use s3::BucketConfiguration;
+
+use warp_constellation::item::{Item, Metadata};
 
 #[derive(Debug, Clone)]
 pub struct StorjClient {
@@ -33,47 +27,59 @@ impl Default for StorjClient {
     fn default() -> Self {
         Self {
             endpoint: String::from("https://gateway.us1.storjshare.io"),
-            ..Default::default()
+            creds: None,
         }
     }
 }
 
 impl StorjClient {
-
     pub fn new<S: AsRef<str>>(access_key: S, secret_key: S, endpoint: String) -> Self {
-        let creds = Some(Credentials{access_key: Some(access_key.as_ref().to_string()), secret_key: Some(secret_key.as_ref().to_string()), security_token: None, session_token: None });
+        let creds = Some(Credentials {
+            access_key: Some(access_key.as_ref().to_string()),
+            secret_key: Some(secret_key.as_ref().to_string()),
+            security_token: None,
+            session_token: None,
+        });
         Self { creds, endpoint }
     }
 
     pub fn set_credentials<S: AsRef<str>>(&mut self, access_key: S, secret_key: S) {
-        let creds = Some(Credentials{access_key: Some(access_key.as_ref().to_string()), secret_key: Some(secret_key.as_ref().to_string()), security_token: None, session_token: None });
+        let creds = Some(Credentials {
+            access_key: Some(access_key.as_ref().to_string()),
+            secret_key: Some(secret_key.as_ref().to_string()),
+            security_token: None,
+            session_token: None,
+        });
         self.creds = creds;
     }
 
     pub async fn bucket<S: AsRef<str>>(&self, bucket: S, create: bool) -> anyhow::Result<Bucket> {
-        
         anyhow::ensure!(self.creds.is_some(), warp_common::error::Error::Other);
 
-        let region = Region::Custom { region: "us-east-1".to_string(), endpoint: self.endpoint.clone() };
+        let region = Region::Custom {
+            region: "us-east-1".to_string(),
+            endpoint: self.endpoint.clone(),
+        };
         let creds = self.creds.clone().unwrap();
         match create {
             true => {
                 let config = BucketConfiguration::default();
-                let create_bucket_response = Bucket::create(bucket.as_ref(), region.clone(), creds.clone(), config).await?;
+                let create_bucket_response =
+                    Bucket::create(bucket.as_ref(), region.clone(), creds.clone(), config).await?;
                 return Ok(create_bucket_response.bucket);
-            },
+            }
             false => {
                 let bucket = Bucket::new(bucket.as_ref(), region, creds)?;
                 Ok(bucket)
             }
         }
-        
     }
-
 }
 
 impl Extension for StorjFilesystem {
-    fn id(&self) -> String { format!("warp-fs-storj") }
+    fn id(&self) -> String {
+        format!("warp-fs-storj")
+    }
 
     fn name(&self) -> String {
         String::from("Storj Filesystem")
@@ -87,40 +93,42 @@ impl Extension for StorjFilesystem {
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(crate = "warp_common::serde")]
 pub struct StorjFilesystem {
-    pub version: ConstellationVersion,
     pub index: Directory,
     pub modified: DateTime<Utc>,
     #[serde(skip)]
-    pub client: StorjClient,
+    pub client: Option<StorjClient>,
     #[serde(skip)]
-    pub cache: Option<Arc<Mutex<Box<dyn PocketDimension>>>>
+    pub cache: Option<Arc<Mutex<Box<dyn PocketDimension>>>>,
 }
 
 impl Default for StorjFilesystem {
-    fn default() -> Self {
+    fn default() -> StorjFilesystem {
         StorjFilesystem {
-            version: ConstellationVersion::from((0, 1, 0)),
             index: Directory::new("root"),
             modified: Utc::now(),
-            ..Default::default()
+            client: None,
+            cache: None,
         }
     }
 }
 
 impl StorjFilesystem {
+    pub fn new<S: AsRef<str>>(access_key: S, secret_key: S) -> StorjFilesystem {
+        let mut system = StorjFilesystem::default();
+        let mut client = StorjClient::default();
+        client.set_credentials(access_key, secret_key);
+        system.client = Some(client);
+
+        system
+    }
 
     pub fn set_cache(&mut self, cache: impl PocketDimension + 'static) {
         self.cache = Some(Arc::new(Mutex::new(Box::new(cache))));
     }
-
 }
 
 #[warp_common::async_trait::async_trait]
 impl Constellation for StorjFilesystem {
-    fn version(&self) -> &ConstellationVersion {
-        &self.version
-    }
-
     fn modified(&self) -> DateTime<Utc> {
         self.modified
     }
@@ -133,47 +141,148 @@ impl Constellation for StorjFilesystem {
         &mut self.index
     }
 
-    async fn put(
-        &mut self,
-        name: &str,
-        path: &str,
-    ) -> warp_common::Result<()> {
-        let mut fs = tokio::fs::File::open(path).await?;
+    /// Uploads file from path with the name format being `bucket_name://path/to/file`
+    /// Note: This only supports uploading of files. This has not implemented creation of
+    ///       directories.
+    /// TODO: Implement cache
+    async fn put(&mut self, name: &str, path: &str) -> warp_common::Result<()> {
+        let name = name.split("://").collect::<Vec<&str>>();
 
+        if name.len() != 2 {
+            return Err(warp_common::error::Error::Other);
+        }
+
+        let (bucket, in_name) = (name.get(0).unwrap(), name.get(1).unwrap());
+
+        let mut fs = tokio::fs::File::open(path).await?;
+        let size = fs.metadata().await?.len();
         //TODO: Allow for custom bucket name
-        let _code = self.client.bucket("root", true).await?.put_object_stream(&mut fs, name).await?;
+        let client = self.client.as_ref().ok_or(Error::Other)?;
+        let code = client
+            .bucket(bucket, true)
+            .await?
+            .put_object_stream(&mut fs, in_name)
+            .await?;
+
+        if code != 200 {
+            return Err(warp_common::error::Error::Other);
+        }
+
+        let mut file = warp_constellation::file::File::new(in_name);
+        file.set_size(size as i64);
+
+        self.open_directory("")?.add_child(file)?;
+        //TODO: Mutate/update the modified date.
+
+        //TODO: Deal with caching that can be more adaptive any ext of pd
+        //Note: Since caching extensions may have different methods of handling
+        //      and storing of data, we would need to look into a streamline
+        //      way of providing data to cache without buffering large amount
+        //      of data to memory.
 
         Ok(())
     }
 
-    async fn get(
-        &self,
-        name: &str,
-        _path: &str,
-    ) -> warp_common::Result<()> {
-        
-        if !self.root_directory().has_child(name) {
+    /// Download file to path with the name format being `bucket_name://path/to/file`
+    /// Note: This only supports uploading of files. This has not implemented creation of
+    ///       directories.
+    /// TODO: Implement cache
+    async fn get(&self, name: &str, path: &str) -> warp_common::Result<()> {
+        let name = name.split("://").collect::<Vec<&str>>();
+
+        if name.len() != 2 {
             return Err(warp_common::error::Error::Other);
         }
 
-        if let Some(cache) = &self.cache {
-            let cache = cache.lock().unwrap();
-            let mut query = QueryBuilder::default();
-            query.r#where("name", name.to_string())?;
-            match cache.get_data(Module::FileSystem, Some(&query)) {
-                Ok(d) => {
-                    //get last
-                    if !d.is_empty() {
-                        let mut list = d.clone();
-                        let _obj = list.pop().unwrap();
-                        
-                        //TODO:
-                        return Ok(());
-                    }
-                }
-                Err(_) => {}
-            }
+        let (bucket, out_name) = (name.get(0).unwrap(), name.get(1).unwrap());
+
+        //TODO: Implement cache
+
+        let _file_size = self
+            .root_directory()
+            .get_child_by_path(out_name)
+            .and_then(Item::get_file)?
+            .size();
+
+        let mut fs = tokio::fs::File::create(path).await?;
+        let client = self.client.as_ref().ok_or(Error::Other)?;
+        let code = client
+            .bucket(bucket, false)
+            .await?
+            .get_object_stream(out_name, &mut fs)
+            .await?;
+
+        if code != 200 {
+            return Err(warp_common::error::Error::Other);
         }
+
+        Ok(())
+    }
+
+    async fn from_buffer(&mut self, name: &str, buffer: &Vec<u8>) -> warp_common::Result<()> {
+        let name = name.split("://").collect::<Vec<&str>>();
+
+        if name.len() != 2 {
+            return Err(warp_common::error::Error::Other);
+        }
+
+        let (bucket, in_name) = (name.get(0).unwrap(), name.get(1).unwrap());
+
+        //TODO: Allow for custom bucket name
+        let client = self.client.as_ref().ok_or(Error::Other)?;
+        let code = client
+            .bucket(bucket, true)
+            .await?
+            .put_object(in_name, &buffer)
+            .await?;
+
+        if code.1 != 200 {
+            return Err(warp_common::error::Error::Other);
+        }
+
+        let mut file = warp_constellation::file::File::new(in_name);
+        file.set_size(buffer.len() as i64);
+
+        self.open_directory("")?.add_child(file)?;
+        //TODO: Mutate/update the modified date.
+
+        //TODO: Deal with caching that can be more adaptive any ext of pd
+        //Note: Since caching extensions may have different methods of handling
+        //      and storing of data, we would need to look into a streamline
+        //      way of providing data to cache without buffering large amount
+        //      of data to memory.
+
+        Ok(())
+    }
+
+    async fn to_buffer(&self, name: &str, buffer: &mut Vec<u8>) -> warp_common::Result<()> {
+        let name = name.split("://").collect::<Vec<&str>>();
+
+        if name.len() != 2 {
+            return Err(warp_common::error::Error::Other);
+        }
+
+        let (bucket, out_name) = (name.get(0).unwrap(), name.get(1).unwrap());
+
+        //TODO: Implement cache
+
+        let _file_size = self
+            .root_directory()
+            .get_child_by_path(out_name)
+            .and_then(Item::get_file)?
+            .size();
+
+        let client = self.client.as_ref().ok_or(Error::Other)?;
+        let code = client
+            .bucket(bucket, false)
+            .await?
+            .get_object(out_name)
+            .await?;
+
+        if code.1 != 200 {
+            return Err(warp_common::error::Error::Other);
+        }
+        *buffer = code.0;
 
         Ok(())
     }
