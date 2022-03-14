03bb1411d8a3b10cada901a42591068d43a2d985
Darius Clark <darius.clark93@gmail.com>
2022-03-14 16:27:04 -0400

    fix(warp): Rename `ConstellationInOutType` to `ConstellationDataType` chore(warp): Reexport `warp_fs_storj` from warp

--- warp/src/lib.rs
+++ warp/src/lib.rs
@@ -1,12 +1,25 @@
-
 #[cfg(feature = "constellation")]
-pub use warp_constellation::{self, constellation::{Constellation, ConstellationInOutType, ConstellationVersion}};
+pub use warp_constellation::{
+    self,
+    constellation::{Constellation, ConstellationDataType, ConstellationVersion},
+};
 #[cfg(feature = "constellation")]
 pub use warp_fs_memory::MemorySystem;
 
-#[cfg(feature = "pocket-dimension")]
-pub use warp_pocket_dimension::{PocketDimension, DimensionDataType, query::{QueryBuilder, Comparator}};
+#[cfg(feature = "constellation")]
+pub use warp_fs_storj::{StorjClient, StorjFilesystem};
+
 #[cfg(feature = "multipass")]
-pub use warp_multipass::{MultiPass, identity::*};
+pub use warp_multipass::{identity::*, MultiPass};
+
+#[cfg(feature = "pocket-dimension")]
+pub use warp_pocket_dimension::{
+    query::{Comparator, QueryBuilder},
+    DimensionDataType, PocketDimension,
+};
+
+#[cfg(feature = "pocket-dimension")]
+pub use warp_pd_stretto::StrettoClient;
+
 #[cfg(feature = "raygun")]
 pub use warp_raygun::{self, RayGun};
