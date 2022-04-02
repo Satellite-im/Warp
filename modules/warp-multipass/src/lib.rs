bc951a78500e032d310ab3479685df6878dc2488
Darius Clark <darius.clark93@gmail.com>
2022-04-02 05:40:45 -0400

    chore(mp): Arrange functions within trait

--- modules/warp-multipass/src/lib.rs
+++ modules/warp-multipass/src/lib.rs
@@ -7,6 +7,8 @@ use warp_data::DataObject;
 use crate::identity::{Identifier, Identity, IdentityUpdate, PublicKey};
 
 pub trait MultiPass: Extension {
+    fn create_identity(&mut self, identity: &Identity, passphrase: &str) -> Result<PublicKey>;
+
     fn get_identity(&self, id: Identifier) -> Result<DataObject>;
 
     fn get_own_identity(&self) -> Result<DataObject> {
@@ -14,8 +16,6 @@ pub trait MultiPass: Extension {
     }
 
     fn update_identity(&mut self, id: Identifier, option: Vec<IdentityUpdate>) -> Result<()>;
-
-    fn create_identity(&mut self, identity: &Identity, passphrase: &str) -> Result<PublicKey>;
 
     fn decrypt_private_key(&self, passphrase: &str) -> Result<Vec<u8>>;
 
