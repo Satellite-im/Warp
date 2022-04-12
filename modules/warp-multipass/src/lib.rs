ec75a58cbf134a40512338a6e9aef7f1f731e17c
Darius Clark <darius.clark93@gmail.com>
2022-04-12 10:45:22 -0400

    refract(mp): Updated `warp-multipass`
    Change `MultiPass::create_identity` to supply a username instead of an identity.
    
    Change `MultiPass::get_identity` to return an Identity.
    
    Change `MultiPass::update_identity` to remove `id` field and change option to be a single type instead of an array.

--- modules/warp-multipass/src/lib.rs
+++ modules/warp-multipass/src/lib.rs
@@ -1,21 +1,21 @@
 pub mod identity;
 
+use identity::Identity;
 use warp_common::Extension;
 use warp_common::Result;
-use warp_data::DataObject;
 
-use crate::identity::{Identifier, Identity, IdentityUpdate, PublicKey};
+use crate::identity::{Identifier, IdentityUpdate, PublicKey};
 
 pub trait MultiPass: Extension + Sync + Send {
-    fn create_identity(&mut self, identity: &mut Identity, passphrase: &str) -> Result<PublicKey>;
+    fn create_identity(&mut self, username: &str, passphrase: &str) -> Result<PublicKey>;
 
-    fn get_identity(&self, id: Identifier) -> Result<DataObject>;
+    fn get_identity(&self, id: Identifier) -> Result<Identity>;
 
-    fn get_own_identity(&self) -> Result<DataObject> {
+    fn get_own_identity(&self) -> Result<Identity> {
         self.get_identity(Identifier::Own)
     }
 
-    fn update_identity(&mut self, id: Identifier, option: Vec<IdentityUpdate>) -> Result<()>;
+    fn update_identity(&mut self, option: IdentityUpdate) -> Result<()>;
 
     fn decrypt_private_key(&self, passphrase: &str) -> Result<Vec<u8>>;
 
