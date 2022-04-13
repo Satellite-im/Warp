2e71598b4cc9e7a98f303030a909d5471c8b6450
Darius Clark <darius.clark93@gmail.com>
2022-04-13 16:15:21 -0400

    chore(multipass): Added a `Friends` trait
    This trait will handle the accepting, denying and management of friends.

--- modules/warp-multipass/src/lib.rs
+++ modules/warp-multipass/src/lib.rs
@@ -23,4 +23,33 @@ pub trait MultiPass: Extension + Sync + Send {
 }
 
 // #[warp_common::async_trait::async_trait]
-pub trait Friends: MultiPass {}
+pub trait Friends {
+    /// Send friend request to corresponding public key
+    fn send_request(&mut self, pubkey: PublicKey) -> Result<()>;
+
+    /// Accept friend request from public key
+    fn accept_request(&mut self, pubkey: PublicKey) -> Result<()>;
+
+    /// Deny friend request from public key
+    fn deny_request(&mut self, pubkey: PublicKey) -> Result<()>;
+
+    /// Closing or retracting friend request
+    fn close_request(&mut self, pubkey: PublicKey) -> Result<()>;
+
+    //TODO: List request
+
+    /// Remove friend from contacts
+    fn remove_friend(&mut self, pubkey: PublicKey) -> Result<()>;
+
+    /// Block public key, rather it be a friend or not, from being able to send request to account public address
+    fn block_key(&mut self, pubkey: PublicKey) -> Result<()>;
+
+    /// List all friends and return a corresponding identity for each public key
+    fn list_friends(&self) -> Result<Vec<Identity>>;
+
+    /// Check to see if public key is friend of the account
+    fn has_friend(&self, pubkey: PublicKey) -> Result<()>;
+
+    /// Returns a diffie-hellman key that is found between one private key and another public key
+    fn key_exchange(&self, identity: Identity) -> Result<Vec<u8>>;
+}
