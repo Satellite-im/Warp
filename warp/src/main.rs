36217b71a411a1b91f0104fdfafbfc1bbe266816
Darius Clark <darius.clark93@gmail.com>
2022-04-25 17:25:06 -0400

    chore(warp): Updated warp crate
    Added flags to allow selecting extensions for different modules
    
    Added commands to upload, download, delete and obtain reference of a file.
    
    Trim the keyfile that is read from whitespaces.

--- warp/src/main.rs
+++ warp/src/main.rs
@@ -45,6 +45,12 @@ struct CommandArgs {
     cli: bool,
     #[clap(short, long)]
     path: Option<String>,
+    #[clap(long)]
+    constellation_module: Option<String>,
+    #[clap(long)]
+    multipass_module: Option<String>,
+    #[clap(long)]
+    pocketdimension_module: Option<String>,
     #[clap(short, long)]
     keyfile: Option<String>,
     #[clap(short, long)]
@@ -53,20 +59,53 @@ struct CommandArgs {
 
 #[derive(Subcommand, Debug)]
 enum Command {
-    Import { key: String, value: String },
-    Export { key: String },
-    Unset { key: String },
+    Import {
+        key: String,
+        value: String,
+    },
+    Export {
+        key: String,
+    },
+    Unset {
+        key: String,
+    },
     Dump,
-    CreateAccount { username: Option<String> },
-    ViewAccount { pubkey: Option<String> },
+    CreateAccount {
+        username: Option<String>,
+    },
+    ViewAccount {
+        pubkey: Option<String>,
+    },
     ListAllRequest,
     ListIncomingRequest,
     ListOutgoingRequest,
     ListFriends,
-    SendFriendRequest { pubkey: String },
-    AcceptFriendRequest { pubkey: String },
-    DenyFriendRequest { pubkey: String },
-    RemoveFriend { pubkey: String },
+    SendFriendRequest {
+        pubkey: String,
+    },
+    AcceptFriendRequest {
+        pubkey: String,
+    },
+    DenyFriendRequest {
+        pubkey: String,
+    },
+    RemoveFriend {
+        pubkey: String,
+    },
+    UploadFile {
+        local: String,
+        remote: Option<String>,
+    },
+    DownloadFile {
+        remote: String,
+        local: String,
+    },
+    DeleteFile {
+        remote: String,
+    },
+    FileReference {
+        remote: String,
+    },
 }
 
 fn default_config() -> warp_configuration::Config {
@@ -101,6 +140,12 @@ fn default_config() -> warp_configuration::Config {
     }
 }
 
+async fn read_file<P: AsRef<Path>>(path: P) -> anyhow::Result<Vec<u8>> {
+    let data = tokio::fs::read_to_string(path).await?;
+    let data = data.trim();
+    Ok(data.as_bytes().to_vec())
+}
+
 #[tokio::main]
 async fn main() -> AnyResult<()> {
     //TODO: Add a logger for outputting to stdout/stderr
@@ -110,14 +155,14 @@ async fn main() -> AnyResult<()> {
     let cli = CommandArgs::parse();
 
     let config = match cli.config {
-        Some(config_path) => Config::load(config_path)?,
+        Some(ref config_path) => Config::load(config_path)?,
         None => default_config(),
     };
 
     let mut manager = ModuleManager::default();
 
     let warp_directory = match cli.path {
-        Some(path) => Path::new(&path).to_path_buf(),
+        Some(ref path) => Path::new(path).to_path_buf(),
         None => warp_common::dirs::home_dir()
             .map(|directory| Path::new(&directory).join(".warp"))
             .ok_or(Error::DirectoryNotFound)?,
@@ -135,10 +180,7 @@ async fn main() -> AnyResult<()> {
 
     //TODO: Have keyfile encrypted
     let mut key = match cli.keyfile {
-        Some(path) => {
-            let data = tokio::fs::read(path).await?;
-            data
-        }
+        Some(ref path) => read_file(path).await?,
         None => cli::password_line()?,
     };
 
@@ -159,7 +201,11 @@ async fn main() -> AnyResult<()> {
         manager.set_cache(Arc::new(Mutex::new(Box::new(storage))));
 
         // get the extension from the config and set it
-        if let Some(cache_ext_name) = config.extensions.pocket_dimension.first() {
+        if let Some(cache_ext_name) = cli
+            .pocketdimension_module
+            .as_ref()
+            .or(config.extensions.pocket_dimension.first())
+        {
             if manager.enable_cache(cache_ext_name).is_err() {
                 warn!("Warning: PocketDimension does not have an active extension.");
             }
@@ -168,8 +214,12 @@ async fn main() -> AnyResult<()> {
 
     if config.modules.constellation {
         let tesseract = tesseract.lock().unwrap();
-        register_fs_ext(&config, &mut manager, &tesseract)?;
-        if let Some(fs_ext) = config.extensions.constellation.first() {
+        register_fs_ext(&cli, &config, &mut manager, &tesseract)?;
+        if let Some(fs_ext) = cli
+            .constellation_module
+            .as_ref()
+            .or(config.extensions.constellation.first())
+        {
             if manager.enable_filesystem(fs_ext).is_err() {
                 warn!("Warning: Constellation does not have an active extension.");
             }
@@ -183,7 +233,11 @@ async fn main() -> AnyResult<()> {
             account.set_cache(cache.clone())
         }
         manager.set_account(Arc::new(Mutex::new(Box::new(account))));
-        if let Some(ext) = config.extensions.multipass.first() {
+        if let Some(ext) = cli
+            .multipass_module
+            .as_ref()
+            .or(config.extensions.multipass.first())
+        {
             if manager.enable_account(ext).is_err() {
                 warn!("Warning: MultiPass does not have an active extension.");
             }
@@ -463,6 +517,87 @@ async fn main() -> AnyResult<()> {
                     &friend.username, &friend.short_id
                 );
             }
+            Command::UploadFile { local, remote } => {
+                let file = Path::new(&local);
+
+                if !file.exists() {
+                    bail!("{} does not exist", file.display());
+                }
+
+                if !file.is_file() {
+                    bail!("{} is not a directory", file.display());
+                }
+
+                let filesystem = manager.get_filesystem()?;
+                let mut filesystem = match filesystem.lock() {
+                    Ok(a) => a,
+                    Err(e) => e.into_inner(),
+                };
+
+                let remote =
+                    remote.unwrap_or(file.file_name().unwrap().to_string_lossy().to_string());
+                filesystem
+                    .put(&remote, &file.to_string_lossy().to_string())
+                    .await?;
+
+                if let Ok(file) = filesystem
+                    .current_directory()
+                    .get_item(&remote)
+                    .and_then(warp::constellation::item::Item::get_file)
+                {
+                    match file.reference() {
+                        Some(r) => println!("{} has been uploaded with {r} as reference", remote),
+                        None => println!("{} has been uploaded", remote),
+                    }
+                }
+            }
+            Command::DownloadFile { remote, local } => {
+                let filesystem = manager.get_filesystem()?;
+                let filesystem = match filesystem.lock() {
+                    Ok(a) => a,
+                    Err(e) => e.into_inner(),
+                };
+
+                match filesystem.get(&remote, &local).await {
+                    Ok(_) => println!("File is downloaded to {local}"),
+                    Err(e) => println!("Error downloading file: {e}"),
+                };
+            }
+            Command::DeleteFile { remote } => {
+                let filesystem = manager.get_filesystem()?;
+                let mut filesystem = match filesystem.lock() {
+                    Ok(a) => a,
+                    Err(e) => e.into_inner(),
+                };
+
+                match filesystem.remove(&remote, true).await {
+                    Ok(_) => println!("{remote} is deleted"),
+                    Err(e) => println!("Error deleting file: {e}"),
+                };
+            }
+            Command::FileReference { remote } => {
+                let filesystem = manager.get_filesystem()?;
+                let mut filesystem = match filesystem.lock() {
+                    Ok(a) => a,
+                    Err(e) => e.into_inner(),
+                };
+
+                match filesystem.sync_ref(&remote).await {
+                    Ok(_) => {}
+                    Err(e) => println!("Warning: Unable to sync reference: {e}"),
+                };
+
+                if let Ok(file) = filesystem
+                    .current_directory()
+                    .get_item(&remote)
+                    .and_then(warp::constellation::item::Item::get_file)
+                {
+                    match file.reference() {
+                        Some(r) => println!("{} Reference: {r}", remote),
+                        None => println!("{} has no reference", remote),
+                    }
+                }
+            }
         },
         _ => warn!("You can only select one option"),
     };
@@ -522,6 +657,7 @@ fn export_to_cache(
 }
 
 fn register_fs_ext(
+    cli: &CommandArgs,
     config: &Config,
     manager: &mut ModuleManager,
     tesseract: &Tesseract,
@@ -539,8 +675,11 @@ fn register_fs_ext(
 
     manager.set_filesystem(Arc::new(Mutex::new(Box::new({
         //TODO: supply passphrase to this function rather than read from cli
-        let (akey, skey) = if let Some("warp-fs-storj") =
-            config.extensions.constellation.first().map(|s| s.as_str())
+        let (akey, skey) = if let Some("warp-fs-storj") = cli
+            .constellation_module
+            .as_ref()
+            .or(config.extensions.constellation.first())
+            .map(|s| s.as_str())
         {
             let akey = tesseract.retrieve("STORJ_ACCESS_KEY")?;
             let skey = tesseract.retrieve("STORJ_SECRET_KEY")?;
