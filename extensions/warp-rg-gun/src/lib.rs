e84302e7459eb7ac6d83434ada258d96b26cbbb4
Darius Clark <darius.clark93@gmail.com>
2022-05-17 13:14:07 -0400

    chore(rg): Update warp-rg-gun
    Note: More testing is needed

--- extensions/warp-rg-gun/src/lib.rs
+++ extensions/warp-rg-gun/src/lib.rs
@@ -1,73 +1,193 @@
 use anyhow::anyhow;
-use gundb::{Node, NodeConfig};
-use std::collections::HashMap;
+use gundb::types::GunValue;
+use gundb::{Config, Node};
+use tokio::sync::mpsc::{Receiver, Sender};
 use warp::sync::{Arc, Mutex, MutexGuard};
 
 use uuid::Uuid;
+use warp::crypto::hash::sha256_hash;
 use warp::error::Error;
 use warp::module::Module;
-use warp::multipass::identity::Identity;
 use warp::multipass::MultiPass;
 use warp::pocket_dimension::PocketDimension;
 use warp::raygun::{
-    Callback, EmbedState, Message, MessageOptions, PinState, RayGun, ReactionState,
+    Callback, EmbedState, Message, MessageOptions, PinState, RayGun, ReactionState, SenderId,
 };
 use warp::Extension;
 
+use serde::{Deserialize, Serialize};
+
 type Result<T> = std::result::Result<T, Error>;
 pub struct GunMessaging {
-    pub account: Option<Arc<Mutex<Box<dyn MultiPass>>>>,
+    pub account: Arc<Mutex<Box<dyn MultiPass>>>,
     pub cache: Option<Arc<Mutex<Box<dyn PocketDimension>>>>,
-    pub conversion: HashMap<Uuid, Vec<Message>>,
+    pub sender: Sender<MessagingEvents>,
+    pub response: Receiver<Result<()>>,
+    pub conversation: Arc<Mutex<Vec<Message>>>,
     pub node: Node,
 }
 
-impl std::fmt::Debug for GunMessaging {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let ident = match self.get_account() {
-            Ok(account) => account.get_own_identity().unwrap_or_default(),
-            Err(_) => Identity::default(),
-        };
-
-        let cache = match self.get_cache() {
-            Ok(cache) => cache.name(),
-            Err(_) => String::from("Unavailable"),
-        };
-
-        f.debug_struct("GunMessaging")
-            .field("account", &ident)
-            .field("cache", &cache)
-            .field("node", &self.node.get_peer_id())
-            .field("conversion", &"<>")
-            .finish()
-    }
-}
-
-impl Default for GunMessaging {
-    fn default() -> Self {
-        Self {
-            account: None,
-            cache: None,
-            node: Node::new(),
-            conversion: HashMap::new(),
-        }
-    }
-}
-
 impl GunMessaging {
-    pub fn new() -> Self {
-        Self::default()
+    pub async fn new(
+        account: Arc<Mutex<Box<dyn MultiPass>>>,
+        cache: Option<Arc<Mutex<Box<dyn PocketDimension>>>>,
+        peers: Vec<String>,
+        port: u16,
+    ) -> Self {
+        let mut gundb_node = Node::new_with_config(Config {
+            outgoing_websocket_peers: peers,
+            sled_storage: false,
+            memory_storage: true,
+            websocket_server: true,
+            websocket_server_port: port,
+            multicast: true,
+            ..Config::default()
+        });
+
+        let node = gundb_node.clone();
+
+        let (sender, mut receiver) = tokio::sync::mpsc::channel(32);
+        let (outer_tx, response) = tokio::sync::mpsc::channel(32);
+
+        let conversation = Arc::new(Mutex::new(Vec::new()));
+        let convo = conversation.clone();
+        tokio::spawn(async move {
+            let mut sub = gundb_node.get(&GunMessaging::node_name().to_string());
+            let mut sub_recv = sub.clone().on();
+            loop {
+                tokio::select! {
+                    event = receiver.recv() => {
+                        match event {
+                            Some(event) => {
+                                let data = match serde_json::to_string(&event) {
+                                    Ok(data) => data,
+                                    Err(e) => {
+                                        if let Err(e) = outer_tx.send(Err(Error::Any(anyhow!(e)))).await {
+                                            //TODO: Log error
+                                            println!("{}", e);
+                                            continue
+                                        }
+                                        continue
+                                    }
+                                };
+                                sub.get(&Self::node_name().to_string()).put(GunValue::Text(data));
+                                        if let Err(e) = outer_tx.send(Ok(())).await {
+                                            //TODO: Log error
+                                            println!("{}", e);
+                                            continue
+                                        }
+
+
+                            },
+                            None => {
+                                continue
+                            }
+                        }
+                    },
+                    n_event = sub_recv.recv() => {
+                        match n_event {
+                            Ok(GunValue::Text(message)) => {
+                                match serde_json::from_str::<MessagingEvents>(&message) {
+                                    Ok(event) => match event {
+                                        MessagingEvents::NewMessage(message) => convo.lock().push(message),
+                                        MessagingEvents::EditMessage(convo_id, message_id, val) => {
+                                            let mut messages = convo.lock();
+
+                                            let index = match messages
+                                                .iter()
+                                                .position(|conv| {
+                                                    conv.conversation_id == convo_id && conv.id == message_id
+                                                })
+                                                .ok_or(Error::ArrayPositionNotFound)
+                                            {
+                                                Ok(index) => index,
+                                                Err(_) => continue,
+                                            };
+
+                                            let message = match messages.get_mut(index) {
+                                                Some(msg) => msg,
+                                                None => continue,
+                                            };
+
+                                            *message.value_mut() = val;
+                                        }
+                                        MessagingEvents::DeleteMessage(convo_id, message_id) => {
+                                            let mut messages = convo.lock();
+
+                                            let index = match messages
+                                                .iter()
+                                                .position(|conv| {
+                                                    conv.conversation_id == convo_id && conv.id == message_id
+                                                })
+                                                .ok_or(Error::ArrayPositionNotFound)
+                                            {
+                                                Ok(index) => index,
+                                                Err(_) => continue,
+                                            };
+
+                                            let _ = messages.remove(index);
+                                        }
+                                        MessagingEvents::PinMessage(convo_id, message_id, state) => {
+                                            let mut messages = convo.lock();
+
+                                            let index = match messages
+                                                .iter()
+                                                .position(|conv| {
+                                                    conv.conversation_id == convo_id && conv.id == message_id
+                                                })
+                                                .ok_or(Error::ArrayPositionNotFound)
+                                            {
+                                                Ok(index) => index,
+                                                Err(_) => continue,
+                                            };
+
+                                            let message = match messages.get_mut(index) {
+                                                Some(msg) => msg,
+                                                None => continue,
+                                            };
+
+                                            match state {
+                                                PinState::Pin => *message.pinned_mut() = true,
+                                                PinState::Unpin => *message.pinned_mut() = false,
+                                            }
+                                        }
+                                        MessagingEvents::ReactMessage(_, _, _) => {}
+                                        MessagingEvents::DeleteConversation(_) => {}
+                                        MessagingEvents::Ping(_) => {}
+                                    },
+                                    Err(e) => {
+                                        println!("Error: {}", e);
+                                        continue
+                                    }
+                                }
+                            },
+                            Ok(_) => {
+                                println!("Incorrect format...");
+                                continue
+                            }
+                            Err(e) => {
+                                println!("{}", e);
+                                continue
+                            },
+                        }
+                    }
+                }
+            }
+        });
+
+        Self {
+            account,
+            cache,
+            sender,
+            response,
+            conversation,
+            node,
+        }
     }
 
-    pub fn new_with_peers(peers: Vec<String>) -> Self {
-        let node = Node::new_with_config(NodeConfig {
-            outgoing_websocket_peers: peers,
-            ..NodeConfig::default()
-        });
-        Self {
-            node,
-            ..Default::default()
-        }
+    pub fn node_name() -> Uuid {
+        let topic_hash = sha256_hash(b"warp-rg-gun", None);
+        Uuid::from_slice(&topic_hash[..topic_hash.len() / 2]).unwrap_or_default()
     }
 
     pub fn get_cache(&self) -> anyhow::Result<MutexGuard<Box<dyn PocketDimension>>> {
@@ -79,13 +199,22 @@ impl GunMessaging {
         Ok(cache.lock())
     }
 
-    pub fn get_account(&self) -> anyhow::Result<MutexGuard<Box<dyn MultiPass>>> {
-        let account = self
-            .account
-            .as_ref()
-            .ok_or_else(|| anyhow!("MultiPass Extension is not set"))?;
+    pub async fn send_event(&mut self, event: MessagingEvents) -> anyhow::Result<()> {
+        self.sender
+            .send(event)
+            .await
+            .map_err(|e| anyhow!("{}", e))?;
 
-        Ok(account.lock())
+        self.response
+            .recv()
+            .await
+            .ok_or_else(|| anyhow!(Error::ToBeDetermined))?
+            .map_err(|e| anyhow!(e))
+    }
+
+    pub fn sender_id(&self) -> anyhow::Result<warp::multipass::identity::PublicKey> {
+        let ident = self.account.lock().get_own_identity()?;
+        Ok(ident.public_key())
     }
 }
 
@@ -103,39 +232,84 @@ impl Extension for GunMessaging {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub enum MessagingEvents {
+    NewMessage(Message),
+    EditMessage(Uuid, Uuid, Vec<String>),
+    DeleteMessage(Uuid, Uuid),
+    DeleteConversation(Uuid),
+    PinMessage(Uuid, Uuid, PinState),
+    ReactMessage(Uuid, Uuid, ReactionState),
+    Ping(Uuid),
+}
+
 #[async_trait::async_trait]
 #[allow(unused_variables)]
 impl RayGun for GunMessaging {
     async fn get_messages(
         &self,
         conversation_id: Uuid,
-        options: MessageOptions,
-        callback: Option<Callback>,
+        _: MessageOptions,
+        _: Option<Callback>,
     ) -> Result<Vec<Message>> {
-        todo!()
+        let messages = self.conversation.lock();
+
+        let list = messages
+            .iter()
+            .filter(|conv| conv.conversation_id == conversation_id)
+            .cloned()
+            .collect::<Vec<Message>>();
+
+        Ok(list)
     }
 
     async fn send(
         &mut self,
         conversation_id: Uuid,
-        message_id: Option<Uuid>,
-        message: Vec<String>,
+        _message_id: Option<Uuid>,
+        value: Vec<String>,
     ) -> Result<()> {
-        todo!()
+        //TODO: Implement editing message
+        //TODO: Check to see if message was sent or if its still sending
+        let pubkey = self.sender_id()?;
+        let mut message = Message::new();
+        message.conversation_id = conversation_id;
+        message.sender = SenderId::from_public_key(pubkey);
+
+        message.value = value;
+
+        self.send_event(MessagingEvents::NewMessage(message.clone()))
+            .await?;
+
+        self.conversation.lock().push(message);
+
+        return Ok(());
     }
 
     async fn delete(&mut self, conversation_id: Uuid, message_id: Uuid) -> Result<()> {
-        todo!()
+        self.send_event(MessagingEvents::DeleteMessage(conversation_id, message_id))
+            .await?;
+
+        let mut messages = self.conversation.lock();
+
+        let index = messages
+            .iter()
+            .position(|conv| conv.conversation_id == conversation_id && conv.id == message_id)
+            .ok_or(Error::ArrayPositionNotFound)?;
+
+        messages.remove(index);
+
+        Ok(())
     }
 
     async fn react(
         &mut self,
-        conversation_id: Uuid,
-        message_id: Uuid,
-        state: ReactionState,
-        emoji: Option<String>,
+        _conversation_id: Uuid,
+        _message_id: Uuid,
+        _state: ReactionState,
+        _emoji: Option<String>,
     ) -> Result<()> {
-        todo!()
+        Err(Error::Unimplemented)
     }
 
     async fn pin(
@@ -144,24 +318,66 @@ impl RayGun for GunMessaging {
         message_id: Uuid,
         state: PinState,
     ) -> Result<()> {
-        todo!()
+        self.send_event(MessagingEvents::PinMessage(
+            conversation_id,
+            message_id,
+            state,
+        ))
+        .await?;
+
+        let mut messages = self.conversation.lock();
+
+        let index = messages
+            .iter()
+            .position(|conv| conv.conversation_id == conversation_id && conv.id == message_id)
+            .ok_or(Error::ArrayPositionNotFound)?;
+
+        let message = messages
+            .get_mut(index)
+            .ok_or(Error::ArrayPositionNotFound)?;
+
+        match state {
+            PinState::Pin => *message.pinned_mut() = true,
+            PinState::Unpin => *message.pinned_mut() = false,
+        }
+
+        Ok(())
+    }
+
+    async fn ping(&mut self, id: Uuid) -> Result<()> {
+        self.send_event(MessagingEvents::Ping(id))
+            .await
+            .map_err(Error::Any)
     }
 
     async fn reply(
         &mut self,
         conversation_id: Uuid,
         message_id: Uuid,
-        message: Vec<String>,
+        value: Vec<String>,
     ) -> Result<()> {
-        todo!()
+        let pubkey = self.sender_id()?;
+        let mut message = Message::new();
+        message.conversation_id = conversation_id;
+        message.replied = Some(message_id);
+        message.sender = SenderId::from_public_key(pubkey);
+
+        message.value = value;
+
+        self.send_event(MessagingEvents::NewMessage(message.clone()))
+            .await?;
+
+        self.conversation.lock().push(message);
+
+        return Ok(());
     }
 
     async fn embeds(
         &mut self,
-        conversation_id: Uuid,
-        message_id: Uuid,
-        state: EmbedState,
+        _conversation_id: Uuid,
+        _message_id: Uuid,
+        _state: EmbedState,
     ) -> Result<()> {
-        todo!()
+        Err(Error::Unimplemented)
     }
 }
