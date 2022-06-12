82b24ba09cf79b3d0e597a8e85ca553fca7b6138
Darius Clark <darius.clark93@gmail.com>
2022-06-12 18:59:40 -0400

    chore(pd): Added examples for memory and stretto and rename flatfile example

---
+++ extensions/warp-pd-stretto/examples/stretto-cache.rs
@@ -0,0 +1,54 @@
+use std::path::PathBuf;
+
+use serde::Deserialize;
+use warp::data::{DataObject, DataType};
+use warp::error::Error;
+use warp::module::Module;
+use warp::pocket_dimension::query::{Comparator, QueryBuilder};
+use warp::pocket_dimension::{DimensionData, PocketDimension};
+use warp_pd_stretto::StrettoClient;
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct Item {
+    pub port: u16,
+    pub data: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let items = vec![
+        Item {
+            port: 10000,
+            data: "Local".into(),
+        },
+        Item {
+            port: 10001,
+            data: "Global".into(),
+        },
+        Item {
+            port: 10002,
+            data: "All".into(),
+        },
+    ];
+
+    let mut cache = StrettoClient::new()?;
+
+    for item in items {
+        let data = DataObject::new(DataType::from(Module::Unknown), item)?;
+
+        cache.add_data(DataType::from(Module::Unknown), &data)?;
+    }
+
+    let mut query = QueryBuilder::default();
+    query.filter(Comparator::Eq, "port", 10001)?;
+
+    let data_list = cache.get_data(DataType::from(Module::Unknown), Some(&query))?;
+
+    for data in data_list {
+        let item = data.payload::<Item>()?;
+        println!("Item::port={}", item.port);
+        println!("Item::data={}", item.data);
+    }
+
+    cache.empty(DataType::from(Module::FileSystem))?;
+    Ok(())
+}
