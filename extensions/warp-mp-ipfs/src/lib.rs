72ece7dc179219e361c577919c349e338f1f8950
Darius Clark <darius.clark93@gmail.com>
2022-07-05 13:22:56 -0400

    chore(mp): Implemented create_identity
    TODO: Test identity creation

--- extensions/warp-mp-ipfs/src/lib.rs
+++ extensions/warp-mp-ipfs/src/lib.rs
@@ -1,7 +1,14 @@
 // Used to ignore unused variables, mostly related to ones in the trait functions
 //TODO: Remove
+//TODO: Use rust-ipfs branch with major changes for pubsub, ipld, etc
 #![allow(unused_variables)]
+#![allow(unused_imports)]
+use anyhow::bail;
+use futures::{Future, TryFutureExt};
+use ipfs::ipld::ipld_macro;
+use serde::de::DeserializeOwned;
 use std::any::Any;
+use std::collections::BTreeMap;
 use std::path::PathBuf;
 use warp::data::{DataObject, DataType};
 use warp::pocket_dimension::query::QueryBuilder;
@@ -12,10 +19,16 @@ use warp::pocket_dimension::PocketDimension;
 use warp::tesseract::Tesseract;
 use warp::{Extension, SingleHandle};
 
-use ipfs::{Ipfs, IpfsOptions, Keypair, TestTypes, Types, UninitializedIpfs};
+use ipfs::ipld::dag_json::DagJsonCodec;
+use ipfs::{
+    make_ipld, Block, Cid, Ipfs, IpfsOptions, IpfsPath, Ipld, Keypair, Types, UninitializedIpfs,
+};
+use libp2p::multihash::Sha2_256;
 use tokio::sync::mpsc::Sender;
+use warp::crypto::rand::Rng;
 use warp::crypto::PublicKey;
 use warp::error::Error;
+use warp::multipass::generator::generate_name;
 use warp::multipass::identity::{FriendRequest, Identifier, Identity, IdentityUpdate};
 use warp::multipass::{identity, Friends, MultiPass};
 
@@ -23,6 +36,7 @@ pub struct IpfsIdentity {
     cache: Option<Arc<Mutex<Box<dyn PocketDimension>>>>,
     tesseract: Tesseract,
     ipfs: Ipfs<Types>,
+    keypair: Keypair,
     //TODO: FriendStore
     //      * Add/Remove/Block friends
     //      * Show incoming/outgoing request
@@ -54,16 +68,16 @@ impl IpfsIdentity {
         tesseract: Tesseract,
         cache: Option<Arc<Mutex<Box<dyn PocketDimension>>>>,
     ) -> anyhow::Result<IpfsIdentity> {
-        let keypair = match tesseract.retrieve("secret") {
+        let keypair = match tesseract.retrieve("ipfs_keypair") {
             Ok(keypair) => {
                 let secret_bytes = bs58::decode(keypair).into_vec()?;
-                let secret = identity::ed25519::SecretKey::from_bytes(&mut sec_key)?;
-                identity::Keypair::Ed25519(secret.into())
+                let secret = libp2p::identity::ed25519::SecretKey::from_bytes(secret_bytes)?;
+                Keypair::Ed25519(secret.into())
             }
             Err(_) => Keypair::generate_ed25519(),
         };
 
-        let mut opts = IpfsOptions {
+        let opts = IpfsOptions {
             ipfs_path: path.unwrap_or_else(|| std::env::temp_dir()),
             keypair: keypair.clone(),
             bootstrap: vec![],
@@ -73,7 +87,7 @@ impl IpfsIdentity {
             span: None,
         };
 
-        let (ipfs, fut): (_, _) = UninitializedIpfs::new(opts).start().await?;
+        let (ipfs, fut) = UninitializedIpfs::new(opts).start().await?;
         tokio::task::spawn(fut);
 
         //TODO: Manually load bootstrap or use IpfsOptions
@@ -83,6 +97,7 @@ impl IpfsIdentity {
             tesseract,
             cache,
             ipfs,
+            keypair,
         })
     }
 
@@ -94,6 +109,29 @@ impl IpfsIdentity {
 
         Ok(cache.lock())
     }
+
+    pub fn raw_keypair(&self) -> anyhow::Result<libp2p::identity::ed25519::Keypair> {
+        match self.keypair.clone() {
+            Keypair::Ed25519(kp) => Ok(kp),
+            _ => bail!("Unsupported keypair"),
+        }
+    }
+}
+
+pub fn async_block<F: Future>(fut: F) -> anyhow::Result<F::Output> {
+    let handle = match tokio::runtime::Handle::try_current() {
+        Ok(handle) => handle,
+        Err(_) => tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()?
+            .handle()
+            .clone(),
+    };
+    Ok(tokio::task::block_in_place(|| handle.block_on(fut)))
+}
+
+pub fn async_block_unchecked<F: Future>(fut: F) -> F::Output {
+    async_block(fut).expect("Unable to run future on runtime")
 }
 
 impl Extension for IpfsIdentity {
@@ -121,7 +159,56 @@ impl MultiPass for IpfsIdentity {
         username: Option<&str>,
         passphrase: Option<&str>,
     ) -> Result<PublicKey, Error> {
-        todo!()
+        if self.tesseract.exist("ipfs_keypair") {
+            //TODO: Check records to determine if profile exist properly
+            if let Ok(cid) = self.tesseract.retrieve("root_cid") {
+                let cid: Cid = cid.parse().map_err(anyhow::Error::from)?;
+                let path = IpfsPath::from(cid);
+                let identity_path = path.sub_path("identity").map_err(anyhow::Error::from)?;
+                let identity_ipld = async_block_unchecked(self.ipfs.get_dag(identity_path))?;
+                //TODO: Perform basic checks against ipld
+                return Err(Error::IdentityExist);
+            }
+        }
+
+        let raw_kp = self.raw_keypair()?;
+
+        let mut identity = Identity::default();
+        let public_key = PublicKey::from_bytes(&raw_kp.public().encode());
+
+        let username = match username {
+            Some(u) => u.to_string(),
+            None => generate_name(),
+        };
+
+        identity.set_username(&username);
+        identity.set_short_id(warp::crypto::rand::thread_rng().gen_range(0, 9999));
+        identity.set_public_key(public_key);
+        // Convert our identity to ipld. This step would convert it to serde_json::Value then match accordingly
+        let ipld_val = to_ipld(identity.clone())?;
+
+        // Store the identity as a dag
+        let ident_cid = async_block_unchecked(self.ipfs.put_dag(make_ipld!(ipld_val)))?;
+        let root_handle =
+            async_block_unchecked(self.ipfs.put_dag(make_ipld!({ "identity": ident_cid })))?;
+
+        // Pin the dag
+        async_block_unchecked(self.ipfs.insert_pin(&root_handle, false))?;
+
+        // Note that for the time being we will be storing the Cid to tesseract,
+        // however this would need to be handled a different way, especially since the cid is stored in the pinstore
+        // in rust-ipfs.
+        // TODO: Store the Cid of the root handle properly
+        // TODO: Provide the Cid to DHT. Either through the PutProvider or (soon to be implemented) ipns
+        self.tesseract.set("root_cid", &root_handle.to_string())?;
+        let encoded_kp = bs58::encode(&raw_kp.encode()).into_string();
+
+        self.tesseract.set("ipfs_keypair", &encoded_kp)?;
+        if let Ok(mut cache) = self.get_cache() {
+            let object = DataObject::new(DataType::from(Module::Accounts), &identity)?;
+            cache.add_data(DataType::from(Module::Accounts), &object)?;
+        }
+        Ok(identity.public_key())
     }
 
     fn get_identity(&self, id: Identifier) -> Result<Identity, Error> {
@@ -131,7 +218,7 @@ impl MultiPass for IpfsIdentity {
             (None, None, true) => {}
             _ => return Err(Error::InvalidIdentifierCondition),
         }
-        todo!()
+        return Err(Error::Unimplemented);
     }
 
     fn update_identity(&mut self, option: IdentityUpdate) -> Result<(), Error> {
@@ -176,6 +263,8 @@ impl MultiPass for IpfsIdentity {
             }
         }
 
+        //TODO: store and broadcast identity
+
         // if let Ok(hooks) = self.get_hooks() {
         //     let object = DataObject::new(DataType::Accounts, identity.clone())?;
         //     hooks.trigger("accounts::update_identity", &object);
@@ -185,7 +274,9 @@ impl MultiPass for IpfsIdentity {
     }
 
     fn decrypt_private_key(&self, passphrase: Option<&str>) -> Result<Vec<u8>, Error> {
-        todo!()
+        self.raw_keypair()
+            .map(|kp| kp.encode().to_vec())
+            .map_err(Error::from)
     }
 
     fn refresh_cache(&mut self) -> Result<(), Error> {
@@ -237,4 +328,74 @@ impl Friends for IpfsIdentity {
     fn has_friend(&self, pubkey: PublicKey) -> Result<(), Error> {
         todo!()
     }
+}
+
+fn to_ipld<S: serde::Serialize>(ser: S) -> anyhow::Result<Ipld> {
+    let value = serde_json::to_value(ser)?;
+    let item = match value {
+        serde_json::Value::Null => Ipld::Null,
+        serde_json::Value::Bool(bool) => Ipld::Bool(bool),
+        serde_json::Value::Number(n) => match (n.as_i64(), n.as_u64(), n.as_f64()) {
+            (Some(n), None, None) => Ipld::Integer(n as i128),
+            (None, Some(n), None) => Ipld::Integer(n as i128),
+            (None, None, Some(n)) => Ipld::Float(n),
+            _ => Ipld::Null,
+        },
+        serde_json::Value::String(string) => Ipld::String(string),
+        serde_json::Value::Array(arr) => {
+            let mut ipld_arr = vec![];
+            for item in arr {
+                ipld_arr.push(to_ipld(item)?)
+            }
+            Ipld::List(ipld_arr)
+        }
+        serde_json::Value::Object(val_map) => {
+            let mut map = BTreeMap::new();
+            for (k, v) in val_map {
+                let ipld = to_ipld(v)?;
+                map.insert(k, ipld);
+            }
+            Ipld::Map(map)
+        }
+    };
+
+    Ok(item)
+}
+
+fn from_ipld<D: DeserializeOwned>(ipld: &Ipld) -> anyhow::Result<D> {
+    let value = match ipld {
+        Ipld::Null => serde_json::Value::Null,
+        Ipld::Bool(bool) => serde_json::Value::Bool(*bool),
+        Ipld::Integer(i) => {
+            if *i >= std::i64::MAX as i128 {
+                //since we dont to convert i128 to i64 if its over the max we will return a null for now
+                serde_json::Value::Null
+            } else {
+                let new_number = *i as i64;
+                serde_json::Value::from(new_number)
+            }
+        }
+        Ipld::Float(float) => serde_json::Value::from(*float),
+        Ipld::String(string) => serde_json::Value::String(string.clone()),
+        Ipld::Bytes(bytes) => serde_json::Value::from(bytes.clone()),
+        Ipld::List(array) => {
+            let mut value_arr = vec![];
+            for item in array {
+                let v = from_ipld(item)?;
+                value_arr.push(v);
+            }
+            serde_json::Value::Array(value_arr)
+        }
+        Ipld::Map(map) => {
+            let mut val_map = serde_json::Map::new();
+            for (k, v) in map {
+                let val = from_ipld(v)?;
+                val_map.insert(k.clone(), val);
+            }
+            serde_json::Value::Object(val_map)
+        }
+        Ipld::Link(_) => serde_json::Value::Null, //Since "Value" doesnt have a cid link, we will leave this null for now
+    };
+    let item = serde_json::from_value(value)?;
+    Ok(item)
 }
