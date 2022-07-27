aeef4404278507e205473a8d5892b74d7ae96800
Darius Clark <darius.clark93@gmail.com>
2022-07-27 10:16:41 -0400

    add(ffi): Implemented couple of functions

--- warp/src/crypto/mod.rs
+++ warp/src/crypto/mod.rs
@@ -144,3 +144,41 @@ pub fn generate(limit: usize) -> Vec<u8> {
     getrandom::getrandom(&mut buf).unwrap();
     buf.to_vec()
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+pub mod ffi {
+    use std::{os::raw::c_char, ffi::{CString, CStr}};
+
+    use crate::{ffi::FFIResult, error::Error};
+
+    use super::DID;
+
+    #[allow(clippy::missing_safety_doc)]
+    #[no_mangle]
+    pub unsafe extern "C" fn did_to_string(
+        did_key: *const DID
+    ) -> *mut c_char {
+        if did_key.is_null() {
+            return std::ptr::null_mut()
+        }
+
+        let did = &*did_key;
+
+        match CString::new(did.to_string()) {
+            Ok(cstr) => cstr.into_raw(),
+            Err(_) => std::ptr::null_mut()
+        }
+    }
+
+    #[allow(clippy::missing_safety_doc)]
+    #[no_mangle]
+    pub unsafe extern "C" fn did_from_string(
+        did_key: *const c_char
+    ) -> FFIResult<DID> {
+        if did_key.is_null() {
+            return FFIResult::err(Error::from(anyhow::anyhow!("did_key is null")));
+        }
+        let did_str = CStr::from_ptr(did_key).to_string_lossy().to_string();
+        did_str.try_into().into()
+    }
+}
