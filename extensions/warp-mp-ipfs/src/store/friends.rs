d277ffce10ef438059953b633861b95ab9f12d98
Darius <darius.clark93@gmail.com>
2022-10-08 11:35:27 -0400

    fix/friends: Use std::fs while saving data when dropping store

--- extensions/warp-mp-ipfs/src/store/friends.rs
+++ extensions/warp-mp-ipfs/src/store/friends.rs
@@ -2,6 +2,7 @@
 use chrono::{DateTime, Utc};
 use futures::{SinkExt, StreamExt, TryFutureExt};
 use ipfs::{Ipfs, IpfsPath, IpfsTypes, Keypair, PeerId, Protocol, Types};
+use std::io::Write;
 use std::ops::Deref;
 use std::path::{Path, PathBuf};
 use std::sync::atomic::{AtomicBool, AtomicU64, AtomicUsize, Ordering};
@@ -145,9 +146,36 @@ impl InternalProfile {
     }
 
     pub fn to_file<P: AsRef<Path>>(&self, path: P, key: &DID) -> anyhow::Result<()> {
-        warp::async_block_in_place_uncheck(self.friends_to_file(&path))?;
-        warp::async_block_in_place_uncheck(self.request_to_file(&path))?;
-        warp::async_block_in_place_uncheck(self.blocks_to_file(&path))?;
+        self.friends_to_file_sync(&path)?;
+        self.request_to_file_sync(&path)?;
+        self.blocks_to_file_sync(&path)?;
+        Ok(())
+    }
+
+    pub fn friends_to_file_sync<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
+        let path = path.as_ref();
+        let bytes = serde_json::to_vec(&self.friends())?;
+        let mut fs = std::fs::File::create(path.join("friends"))?;
+        fs.write_all(&bytes)?;
+        fs.flush()?;
+        Ok(())
+    }
+
+    pub fn blocks_to_file_sync<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
+        let path = path.as_ref();
+        let bytes = serde_json::to_vec(&self.block_list())?;
+        let mut fs = std::fs::File::create(path.join("block_list"))?;
+        fs.write_all(&bytes)?;
+        fs.flush()?;
+        Ok(())
+    }
+
+    pub fn request_to_file_sync<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
+        let path = path.as_ref();
+        let bytes = serde_json::to_vec(&self.requests())?;
+        let mut fs = std::fs::File::create(path.join("request_list"))?;
+        fs.write_all(&bytes)?;
+        fs.flush()?;
         Ok(())
     }
 
