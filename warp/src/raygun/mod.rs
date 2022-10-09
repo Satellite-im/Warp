34115391758c159f1808f41fe798083d6ee90fd2
Darius <darius.clark93@gmail.com>
2022-10-09 02:39:26 -0400

    add: Added FFI functions for raygun to create and access stream

--- warp/src/raygun/mod.rs
+++ warp/src/raygun/mod.rs
@@ -664,16 +664,20 @@ impl RayGunAdapter {
 
 #[cfg(not(target_arch = "wasm32"))]
 pub mod ffi {
-    use super::{Conversation, ConversationType, FFIResult_FFIVec_Conversation};
+    use super::{
+        Conversation, ConversationType, FFIResult_FFIVec_Conversation, MessageEventStream,
+        RayGunEventStream,
+    };
     use crate::async_on_block;
     use crate::crypto::{FFIVec_DID, DID};
     use crate::error::Error;
-    use crate::ffi::{FFIResult, FFIResult_Null, FFIVec_String};
+    use crate::ffi::{FFIResult, FFIResult_Null, FFIResult_String, FFIVec_String};
     use crate::raygun::{
         EmbedState, FFIResult_FFIVec_Message, FFIVec_Reaction, Message, MessageOptions, PinState,
         RayGunAdapter, Reaction, ReactionState,
     };
     use chrono::{DateTime, NaiveDateTime, Utc};
+    use futures::StreamExt;
     use std::ffi::{CStr, CString};
     use std::os::raw::c_char;
     use std::str::{FromStr, Utf8Error};
@@ -1040,6 +1044,81 @@ pub mod ffi {
 
         let adapter = &mut *ctx;
         async_on_block(adapter.write_guard().embeds(convo_id, msg_id, state)).into()
+    }
+
+    #[allow(clippy::missing_safety_doc)]
+    #[no_mangle]
+    pub unsafe extern "C" fn raygun_subscribe(
+        ctx: *mut RayGunAdapter,
+    ) -> FFIResult<RayGunEventStream> {
+        if ctx.is_null() {
+            return FFIResult::err(Error::Any(anyhow::anyhow!("Context cannot be null")));
+        }
+
+        let rg = &mut *(ctx);
+
+        async_on_block(rg.write_guard().subscribe()).into()
+    }
+
+    #[allow(clippy::missing_safety_doc)]
+    #[no_mangle]
+    pub unsafe extern "C" fn raygun_stream_next(ctx: *mut RayGunEventStream) -> FFIResult_String {
+        if ctx.is_null() {
+            return FFIResult_String::err(Error::Any(anyhow::anyhow!("Context cannot be null")));
+        }
+
+        let stream = &mut *(ctx);
+
+        match async_on_block(stream.next()) {
+            Some(event) => serde_json::to_string(&event).map_err(Error::from).into(),
+            None => FFIResult_String::err(Error::Any(anyhow::anyhow!(
+                "Error obtaining data from stream"
+            ))),
+        }
+    }
+
+    #[allow(clippy::missing_safety_doc)]
+    #[no_mangle]
+    pub unsafe extern "C" fn raygun_get_conversation_stream(
+        ctx: *mut RayGunAdapter,
+        conversation_id: *const c_char,
+    ) -> FFIResult<MessageEventStream> {
+        if ctx.is_null() {
+            return FFIResult::err(Error::Any(anyhow::anyhow!("Context cannot be null")));
+        }
+
+        if conversation_id.is_null() {
+            return FFIResult::err(Error::Any(anyhow::anyhow!(
+                "Conversation ID cannot be null"
+            )));
+        }
+
+        let conversation_id =
+            match Uuid::from_str(&CStr::from_ptr(conversation_id).to_string_lossy()) {
+                Ok(uuid) => uuid,
+                Err(e) => return FFIResult::err(Error::UuidError(e)),
+            };
+
+        let rg = &mut *(ctx);
+
+        async_on_block(rg.write_guard().get_conversation_stream(conversation_id)).into()
+    }
+
+    #[allow(clippy::missing_safety_doc)]
+    #[no_mangle]
+    pub unsafe extern "C" fn message_stream_next(ctx: *mut MessageEventStream) -> FFIResult_String {
+        if ctx.is_null() {
+            return FFIResult_String::err(Error::Any(anyhow::anyhow!("Context cannot be null")));
+        }
+
+        let stream = &mut *(ctx);
+
+        match async_on_block(stream.next()) {
+            Some(event) => serde_json::to_string(&event).map_err(Error::from).into(),
+            None => FFIResult_String::err(Error::Any(anyhow::anyhow!(
+                "Error obtaining data from stream"
+            ))),
+        }
     }
 
     #[allow(clippy::missing_safety_doc)]
