cb47257ffd6f560b912284acd0f5930538a9872c
Darius <darius.clark93@gmail.com>
2022-10-17 17:57:35 -0400

    chore: Use path for log if available

--- extensions/warp-mp-ipfs/examples/identity-interface.rs
+++ extensions/warp-mp-ipfs/examples/identity-interface.rs
@@ -109,17 +109,20 @@ async fn account_persistent<P: AsRef<Path>>(
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
     if fdlimit::raise_fd_limit().is_none() {}
 
-    let file_appender = tracing_appender::rolling::hourly("./", "warp_mp_identity_interface.log");
+    let file_appender = match &opt.path {
+        Some(path) => tracing_appender::rolling::hourly(path, "warp_mp_identity_interface.log"),
+        None => tracing_appender::rolling::hourly("./", "warp_mp_identity_interface.log"),
+    };
+
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
 
     tracing_subscriber::fmt()
         .with_writer(non_blocking)
         .with_env_filter(EnvFilter::from_default_env())
         .init();
-
-    let opt = Opt::parse();
 
     let cache = cache_setup(opt.path.clone()).ok();
 
