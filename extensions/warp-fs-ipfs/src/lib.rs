3408f5fa50ee608dcde062f6c6db87efc866dbf2
lgmarchi <lucasgmarchi@gmail.com>
2022-11-25 18:15:55 -0300

    refactor(File): Check new name

--- extensions/warp-fs-ipfs/src/lib.rs
+++ extensions/warp-fs-ipfs/src/lib.rs
@@ -670,7 +670,7 @@ impl<T: IpfsTypes> Constellation for IpfsFileSystem<T> {
         //Note: This will only support renaming the file or directory in the index
         let directory = self.current_directory()?;
 
-        if directory.get_item_by_path(name).is_ok() {
+        if directory.get_item_by_path(new).is_ok() {
             return Err(Error::DuplicateName);
         }
 
