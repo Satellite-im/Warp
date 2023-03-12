e4eb34d754e0cbfac0a7d6f639c42f7ce6769813
Darius Clark <dariusc93@users.noreply.github.com>
2023-03-12 19:34:07 -0400

    fix: Save when setting only if file doesnt exist (#199)

--- warp/src/tesseract/mod.rs
+++ warp/src/tesseract/mod.rs
@@ -226,7 +226,9 @@ impl Tesseract {
     /// ```
     pub fn set_file<P: AsRef<Path>>(&self, file: P) {
         *self.file.write() = Some(file.as_ref().to_path_buf());
-        if let Err(_e) = self.to_file(file) {}
+        if !file.as_ref().is_file() {
+            if let Err(_e) = self.to_file(file) {}
+        }
     }
 
     /// Internal file handle
