917027a37b1d5a0b1e2da43f465464f6a8ba61d5
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-05-17 13:04:45 -0400

    add function to record audio input

--- tools/audio-kit/src/main.rs
+++ tools/audio-kit/src/main.rs
@@ -1,8 +1,12 @@
-use std::{mem, slice};
+use std::{mem, slice, time::Duration};
 
 use anyhow::bail;
 use bytes::Bytes;
 use clap::Parser;
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    SampleRate,
+};
 use once_cell::sync::Lazy;
 use tokio::sync::Mutex;
 
@@ -11,6 +15,8 @@ use tokio::sync::Mutex;
 enum Cli {
     /// Sets the sample type to be used by CPAL
     SampleType { sample_type: SampleTypes },
+    /// the CPAL buffer size
+    // BufferSize {buffer_size: u32},
     /// sets the number of bits per second in the opus encoder.
     /// accepted values range from 500 to 512000 bits per second
     BitRate { rate: i32 },
@@ -19,7 +25,7 @@ enum Cli {
     SampleRate { rate: u32 },
     /// sets the number of samples per channel in the input signal.
     /// accepted values are 120, 240, 480, 960, 1920, and 2880
-    FrameSize { frame_size: u32 },
+    FrameSize { frame_size: usize },
     /// sets the opus::Bandwidth. values are 1101 (4khz), 1102 (6kHz), 1103 (8kHz), 1104 (12kHz), and 1105 (20kHz)
     /// the kHz values represent the range of a bandpass filter
     Bandwidth { bandwidth: i32 },
@@ -29,6 +35,8 @@ enum Cli {
     Record,
     /// plays the most recently recorded audio
     Play,
+    /// print the current config
+    ShowConfig,
 }
 
 #[derive(Debug, Clone, clap::ValueEnum)]
@@ -44,13 +52,21 @@ struct StaticArgs {
     sample_type: SampleTypes,
     bit_rate: opus::Bitrate,
     sample_rate: u32,
-    frame_size: u32,
+    frame_size: usize,
     bandwidth: opus::Bandwidth,
     application: opus::Application,
 }
+
+struct EncodedSamples {
+    data: Vec<u8>,
+    idx: usize,
+}
 /// this will be used for audio processing. not going to put this in a mutex or send all the audio samples through a channel
 /// because that's probably too slow.
-static mut ENCODED_BYTES: Vec<u8> = Vec::new();
+static mut ENCODED_SAMPLES: EncodedSamples = EncodedSamples {
+    data: vec![],
+    idx: 0,
+};
 
 static STATIC_MEM: Lazy<Mutex<StaticArgs>> = Lazy::new(|| {
     Mutex::new(StaticArgs {
@@ -102,9 +118,66 @@ async fn handle_command(cli: Cli) -> anyhow::Result<()> {
                 _ => bail!("invalid application"),
             };
         }
-        Cli::Record => {}
+        Cli::Record => record_f32(sm.clone()).await?,
         Cli::Play => {}
+        Cli::ShowConfig => println!("{:#?}", sm),
     }
+    Ok(())
+}
+
+async fn record_f32(args: StaticArgs) -> anyhow::Result<()> {
+    let duration_secs = 5;
+    let total_samples = args.sample_rate as usize * 4 * (duration_secs + 1);
+    unsafe {
+        ENCODED_SAMPLES.data.resize(total_samples, 0);
+        ENCODED_SAMPLES.idx = 0;
+    }
+    let config = cpal::StreamConfig {
+        channels: 1,
+        sample_rate: SampleRate(args.sample_rate),
+        buffer_size: cpal::BufferSize::Default,
+    };
+    let mut packetizer =
+        OpusPacketizer::init(args.frame_size, args.sample_rate, opus::Channels::Mono)?;
+
+    // batch audio samples into a Packetizer, encode them via packetize(), and write the bytes to a global variable.
+    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| unsafe {
+        // if there isn't space for a new frame, then stop
+        if ENCODED_SAMPLES.idx >= total_samples - args.frame_size {
+            log::error!("ran out of space for samples");
+            return;
+        }
+        let mut encoded: &mut [u8] =
+            &mut ENCODED_SAMPLES.data.as_mut_slice()[ENCODED_SAMPLES.idx..];
+        for sample in data {
+            let r = match packetizer.packetize_f32(*sample, &mut encoded) {
+                Ok(r) => r,
+                Err(e) => {
+                    log::error!("failed to packetize: {e}");
+                    continue;
+                }
+            };
+            if let Some(size) = r {
+                ENCODED_SAMPLES.idx += size;
+            }
+        }
+    };
+    let input_stream = cpal::default_host()
+        .default_input_device()
+        .ok_or(anyhow::anyhow!("no input device"))?
+        .build_input_stream(&config.into(), input_data_fn, err_fn, None)
+        .map_err(|e| {
+            anyhow::anyhow!(
+                "failed to build input stream: {e}, {}, {}",
+                file!(),
+                line!()
+            )
+        })?;
+
+    input_stream.play()?;
+    tokio::time::sleep(Duration::from_secs(duration_secs as u64)).await;
+    input_stream.pause()?;
+
     Ok(())
 }
 
@@ -165,7 +238,7 @@ impl OpusPacketizer {
         })
     }
 
-    fn packetize_i16(&mut self, sample: i16, out: &mut [u8]) -> anyhow::Result<usize> {
+    fn packetize_i16(&mut self, sample: i16, out: &mut [u8]) -> anyhow::Result<Option<usize>> {
         // opus::Encoder::encode is using raw pointers under the hood.
         let p: *const i16 = &sample;
         let bp: *const u8 = p as *const _;
@@ -180,16 +253,16 @@ impl OpusPacketizer {
                 Ok(size) => {
                     self.raw_bytes.clear();
                     self.num_samples = 0;
-                    return Ok(size);
+                    return Ok(Some(size));
                 }
                 Err(e) => anyhow::bail!("failed to encode: {e}"),
             }
         } else {
-            return Ok(0);
+            return Ok(None);
         }
     }
 
-    fn packetize_f32(&mut self, sample: f32, out: &mut [u8]) -> anyhow::Result<usize> {
+    fn packetize_f32(&mut self, sample: f32, out: &mut [u8]) -> anyhow::Result<Option<usize>> {
         // opus::Encoder::encode is using raw pointers under the hood.
         let p: *const f32 = &sample;
         let bp: *const u8 = p as *const _;
@@ -204,12 +277,16 @@ impl OpusPacketizer {
                 Ok(size) => {
                     self.raw_bytes.clear();
                     self.num_samples = 0;
-                    return Ok(size);
+                    return Ok(Some(size));
                 }
                 Err(e) => anyhow::bail!("failed to encode: {e}"),
             }
         } else {
-            return Ok(0);
+            return Ok(None);
         }
     }
+}
+
+fn err_fn(err: cpal::StreamError) {
+    log::error!("an error occurred on stream: {}", err);
 }
