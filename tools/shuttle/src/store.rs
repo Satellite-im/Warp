0a06dadff7e881b1b184dfb0d401528f36f414ef
Darius <darius.clark@proton.me>
2023-05-21 01:29:59 -0400

    chore: Update store interface

--- tools/shuttle/src/store.rs
+++ tools/shuttle/src/store.rs
@@ -4,18 +4,13 @@ pub mod ipfs;
 
 #[async_trait::async_trait]
 pub trait Store: Send + Sync + 'static {
-    type Type;
-    type Channel;
-
-    fn store_type(&self) -> Self::Type;
-
     async fn insert(&mut self, key: &[u8], value: &[u8]) -> Result<(), anyhow::Error>;
 
     async fn find<'a>(&self, key: &[u8]) -> Result<Cow<'a, [u8]>, anyhow::Error>;
 
+    async fn find_all<'a>(&self, key: &[u8]) -> Result<Vec<Cow<'a, [u8]>>, anyhow::Error>;
+
     async fn replace(&mut self, key: &[u8], value: &[u8]) -> Result<(), anyhow::Error>;
 
     async fn remove(&mut self, key: &[u8]) -> Result<(), anyhow::Error>;
-
-    async fn watch(&self) -> Self::Channel;
 }
