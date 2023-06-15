deba3d38996b4d94e5e323408b023e7a42b4ca48
Darius <darius.clark@proton.me>
2023-06-15 10:05:44 -0400

    fix: Access stream if account isnt ready

--- extensions/warp-fs-ipfs/src/lib.rs
+++ extensions/warp-fs-ipfs/src/lib.rs
@@ -83,7 +83,7 @@ impl IpfsFileSystem {
             let mut filesystem = filesystem.clone();
             let account = filesystem.account.clone();
             async move {
-                if account.is_ready() {
+                if !account.is_ready() {
                     let Ok(mut stream) = account.extension_subscribe() else {
                         return
                     };
