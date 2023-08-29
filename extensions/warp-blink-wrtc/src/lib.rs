244c0e2d858b8be7ecb1f89dc7580ae0f49a560f
sdwoodbury <stuartwoodbury@protonmail.ch>
2023-08-29 15:15:22 -0400

    feat(blink): emit event when a call is automatically terminated (#311)

--- extensions/warp-blink-wrtc/src/lib.rs
+++ extensions/warp-blink-wrtc/src/lib.rs
@@ -135,6 +135,7 @@ impl Drop for BlinkImpl {
             }
             host_media::audio::automute::stop();
             host_media::reset().await;
+            //rtp_logger::deinit().await;
             log::debug!("deinit finished");
         });
     }
@@ -266,6 +267,7 @@ impl BlinkImpl {
     }
 
     async fn init_call(&mut self, call: CallInfo) -> anyhow::Result<()> {
+        //rtp_logger::init(call.call_id(), std::path::PathBuf::from("")).await?;
         let lock = self.ipfs.read().await;
         let ipfs = match lock.as_ref() {
             Some(r) => r,
@@ -678,13 +680,17 @@ async fn handle_webrtc(params: WebRtcHandlerParams, mut webrtc_event_stream: Web
                                 }
                                 // have to use data after active_call or there will be 2 mutable borrows, which isn't allowed
                                 webrtc_controller.hang_up(&peer).await;
-                                // this log should appear after the logs emitted by hang_up
-                                if all_closed {
+                                // only autoclose for 2-person calls (group or direct).
+                                // library user should respond to CallTerminated event.
+                                if all_closed && active_call.call.participants().len() == 2 {
                                     log::info!("all participants have successfully been disconnected");
                                     if let Err(e) = webrtc_controller.deinit().await {
                                         log::error!("webrtc deinit failed: {e}");
                                     }
+                                    //rtp_logger::deinit().await;
                                     host_media::reset().await;
+                                    let event = BlinkEventKind::CallTerminated { call_id };
+                                    let _ = ch.send(event);
                                     // terminate the task on purpose.
                                     return;
                                 }
@@ -981,6 +987,7 @@ impl Blink for BlinkImpl {
 
             let r = self.webrtc_controller.write().await.deinit().await;
             host_media::reset().await;
+            //rtp_logger::deinit().await;
             let _ = r?;
             Ok(())
         } else {
