1313d49d71d9e599276c02f6bd32daf7bbd41c8f
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-09-01 14:16:37 -0400

    silence noisy log

--- tools/blink-cli/src/main.rs
+++ tools/blink-cli/src/main.rs
@@ -258,7 +258,10 @@ async fn handle_command(
 async fn handle_blink_event_stream(mut stream: BlinkEventStream) -> anyhow::Result<()> {
     while let Some(evt) = stream.next().await {
         // get rid of noisy logs
-        if !matches!(evt, BlinkEventKind::ParticipantSpeaking { .. }) {
+        if !matches!(
+            evt,
+            BlinkEventKind::ParticipantSpeaking { .. } | BlinkEventKind::SelfSpeaking
+        ) {
             println!("BlinkEvent: {evt}");
         }
 
