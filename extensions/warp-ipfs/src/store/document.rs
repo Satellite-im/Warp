47cd3281df5dfb7571ac51d54e1d4b8c41f8407c
Darius <darius.clark@proton.me>
2023-10-09 00:58:57 -0400

    chore: Simplify logic when resolving root document

--- extensions/warp-ipfs/src/store/document.rs
+++ extensions/warp-ipfs/src/store/document.rs
@@ -1,6 +1,7 @@
 pub mod identity;
 pub mod utils;
 
+use futures::TryFutureExt;
 use ipfs::{Ipfs, IpfsPath};
 use libipld::{
     serde::{from_ipld, to_ipld},
@@ -132,14 +133,20 @@ impl RootDocument {
 
     #[tracing::instrument(skip(self, ipfs))]
     pub async fn verify(&self, ipfs: &Ipfs) -> Result<(), Error> {
-        let (identity, _, _, _, _) = self.resolve(ipfs).await?;
+        let identity: IdentityDocument = self
+            .identity
+            .get_local_dag(ipfs)
+            .await
+            .map_err(|_| Error::IdentityInvalid)?;
+
         let mut root_document = self.clone();
         let signature =
             std::mem::take(&mut root_document.signature).ok_or(Error::InvalidSignature)?;
         let bytes = serde_json::to_vec(&root_document)?;
         let sig = bs58::decode(&signature).into_vec()?;
+
         identity
-            .did_key()
+            .did
             .verify(&bytes, &sig)
             .map_err(|_| Error::InvalidSignature)?;
         Ok(())
@@ -151,38 +158,33 @@ impl RootDocument {
         &self,
         ipfs: &Ipfs,
     ) -> Result<(Identity, Vec<DID>, Vec<DID>, Vec<DID>, Vec<Request>), Error> {
-        let identity = match ipfs
-            .dag()
-            .get(IpfsPath::from(self.identity), &[], true)
+        let document: IdentityDocument = self
+            .identity
+            .get_local_dag(ipfs)
             .await
-        {
-            Ok(ipld) => from_ipld::<IdentityDocument>(ipld)
-                .map_err(anyhow::Error::from)
-                .map_err(Error::from)?
-                .resolve()?,
-            Err(_) => return Err(Error::IdentityInvalid),
-        };
+            .map_err(|_| Error::IdentityInvalid)?;
 
-        let mut friends = Default::default();
-        let mut block_list = Default::default();
-        let mut block_by_list = Default::default();
-        let mut request = Default::default();
+        let identity = document.resolve()?;
 
-        if let Some(document) = &self.friends {
-            friends = document.get_local_dag(ipfs).await.unwrap_or_default();
-        }
+        let friends = futures::future::ready(self.friends.ok_or(Error::Other))
+            .and_then(|document| async move { document.get_local_dag(ipfs).await })
+            .await
+            .unwrap_or_default();
 
-        if let Some(document) = &self.blocks {
-            block_list = document.get_local_dag(ipfs).await.unwrap_or_default();
-        }
+        let block_list = futures::future::ready(self.blocks.ok_or(Error::Other))
+            .and_then(|document| async move { document.get_local_dag(ipfs).await })
+            .await
+            .unwrap_or_default();
 
-        if let Some(document) = &self.block_by {
-            block_by_list = document.get_local_dag(ipfs).await.unwrap_or_default();
-        }
+        let block_by_list = futures::future::ready(self.block_by.ok_or(Error::Other))
+            .and_then(|document| async move { document.get_local_dag(ipfs).await })
+            .await
+            .unwrap_or_default();
 
-        if let Some(document) = &self.request {
-            request = document.get_local_dag(ipfs).await.unwrap_or_default();
-        }
+        let request = futures::future::ready(self.request.ok_or(Error::Other))
+            .and_then(|document| async move { document.get_local_dag(ipfs).await })
+            .await
+            .unwrap_or_default();
 
         Ok((identity, friends, block_list, block_by_list, request))
     }
