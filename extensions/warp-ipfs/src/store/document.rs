299da4a7c878f7c786c00e0a17addcae505eaa0e
Darius Clark <dariusc93@users.noreply.github.com>
2023-10-20 16:28:10 -0400

    refactor: Move conversation functionality into its own task (#340)

--- extensions/warp-ipfs/src/store/document.rs
+++ extensions/warp-ipfs/src/store/document.rs
@@ -1,4 +1,5 @@
 pub mod cache;
+pub mod conversation;
 pub mod identity;
 pub mod root;
 pub mod utils;
@@ -12,7 +13,8 @@ use libipld::{
 };
 use rust_ipfs as ipfs;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use std::time::Duration;
+use std::{collections::BTreeMap, str::FromStr, time::Duration};
+use uuid::Uuid;
 use warp::{
     crypto::{did_key::CoreSign, DID},
     error::Error,
@@ -23,7 +25,7 @@ use crate::store::get_keypair_did;
 
 use self::{identity::IdentityDocument, utils::GetLocalDag};
 
-use super::identity::Request;
+use super::{identity::Request, keystore::Keystore};
 
 #[async_trait::async_trait]
 pub(crate) trait ToCid: Sized {
@@ -83,6 +85,7 @@ pub struct ExtractedRootDocument {
     pub block_list: Vec<DID>,
     pub block_by_list: Vec<DID>,
     pub request: Vec<super::identity::Request>,
+    pub conversation_keystore: BTreeMap<Uuid, Keystore>,
     pub signature: Option<Vec<u8>>,
 }
 
@@ -123,6 +126,12 @@ pub struct RootDocument {
     /// array of request (Request)
     #[serde(skip_serializing_if = "Option::is_none")]
     pub request: Option<Cid>,
+    /// map of conversations
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub conversations: Option<Cid>,
+    /// map of keystore for group chat conversations
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub conversations_keystore: Option<Cid>,
     /// Online/Away/Busy/Offline status
     #[serde(skip_serializing_if = "Option::is_none")]
     pub status: Option<IdentityStatus>,
@@ -182,6 +191,7 @@ impl RootDocument {
             Vec<DID>,
             Vec<DID>,
             Vec<Request>,
+            BTreeMap<Uuid, Keystore>,
         ),
         Error,
     > {
@@ -213,6 +223,24 @@ impl RootDocument {
             .await
             .unwrap_or_default();
 
+        let conversation_keystore =
+            futures::future::ready(self.conversations_keystore.ok_or(Error::Other))
+                .and_then(|document| async move {
+                    let map: BTreeMap<String, Cid> = document.get_local_dag(ipfs).await?;
+                    let mut resolved_map: BTreeMap<Uuid, Keystore> = BTreeMap::new();
+                    for (k, v) in map
+                        .iter()
+                        .filter_map(|(k, v)| Uuid::from_str(k).map(|k| (k, *v)).ok())
+                    {
+                        if let Ok(store) = v.get_local_dag(ipfs).await {
+                            resolved_map.insert(k, store);
+                        }
+                    }
+                    Ok(resolved_map)
+                })
+                .await
+                .unwrap_or_default();
+
         Ok((
             identity,
             self.created,
@@ -221,6 +249,7 @@ impl RootDocument {
             block_list,
             block_by_list,
             request,
+            conversation_keystore,
         ))
     }
 
@@ -239,6 +268,7 @@ impl RootDocument {
         let has_blocks = !data.block_list.is_empty();
         let has_block_by_list = !data.block_by_list.is_empty();
         let has_requests = !data.request.is_empty();
+        let has_keystore = !data.conversation_keystore.is_empty();
 
         let friends = has_friends
             .then_some(data.friends.to_cid(ipfs).await.ok())
@@ -253,10 +283,24 @@ impl RootDocument {
             .then_some(data.request.to_cid(ipfs).await.ok())
             .flatten();
 
+        let conversations_keystore = has_keystore
+            .then_some({
+                let mut pointer_map: BTreeMap<String, Cid> = BTreeMap::new();
+                for (k, v) in data.conversation_keystore {
+                    if let Ok(cid) = v.to_cid(ipfs).await {
+                        pointer_map.insert(k.to_string(), cid);
+                    }
+                }
+                pointer_map.to_cid(ipfs).await.ok()
+            })
+            .flatten();
+
         let root_document = RootDocument {
             identity,
             created: Some(data.created),
             modified: Some(data.modified),
+            conversations: None,
+            conversations_keystore,
             friends,
             blocks,
             block_by,
@@ -270,8 +314,16 @@ impl RootDocument {
     }
 
     pub async fn export(&self, ipfs: &Ipfs) -> Result<ExtractedRootDocument, Error> {
-        let (identity, created, modified, friends, block_list, block_by_list, request) =
-            self.resolve(ipfs).await?;
+        let (
+            identity,
+            created,
+            modified,
+            friends,
+            block_list,
+            block_by_list,
+            request,
+            conversation_keystore,
+        ) = self.resolve(ipfs).await?;
 
         let mut exported = ExtractedRootDocument {
             identity,
@@ -281,6 +333,7 @@ impl RootDocument {
             block_list,
             block_by_list,
             request,
+            conversation_keystore,
             signature: None,
         };
 
@@ -293,130 +346,3 @@ impl RootDocument {
         Ok(exported)
     }
 }
-
-// Note: This is commented out temporarily due to a race condition that was found while testing. This may get reenabled and used in the near future
-// #[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-// pub struct ConversationRootDocument {
-//     pub did: DID,
-//     pub conversations: HashSet<DocumentType<ConversationDocument>>,
-// }
-
-// impl ConversationRootDocument {
-//     pub fn new(did: DID) -> Self {
-//         Self {
-//             did,
-//             conversations: Default::default(),
-//         }
-//     }
-// }
-
-// impl ConversationRootDocument {
-//     pub async fn get_conversation(
-//         &self,
-//         ipfs: Ipfs,
-//         conversation_id: Uuid,
-//     ) -> Result<ConversationDocument, Error> {
-//         let document_type = self
-//             .get_conversation_document(ipfs.clone(), conversation_id)
-//             .await?;
-//         document_type.resolve(ipfs, None).await
-//     }
-
-//     pub async fn get_conversation_document(
-//         &self,
-//         ipfs: Ipfs,
-//         conversation_id: Uuid,
-//     ) -> Result<DocumentType<ConversationDocument>, Error> {
-//         FuturesUnordered::from_iter(self.conversations.iter().map(|document| {
-//             let ipfs = ipfs.clone();
-//             async move {
-//                 let document_type = document.clone();
-//                 document
-//                     .resolve(ipfs, None)
-//                     .await
-//                     .map(|document| (document_type, document))
-//             }
-//         }))
-//         .filter_map(|result| async { result.ok() })
-//         .filter(|(_, document)| {
-//             let id = document.id;
-//             async move { id == conversation_id }
-//         })
-//         .map(|(document_type, _)| document_type)
-//         .collect::<Vec<_>>()
-//         .await
-//         .first()
-//         .cloned()
-//         .ok_or(Error::InvalidConversation)
-//     }
-
-//     pub async fn list_conversations(
-//         &self,
-//         ipfs: Ipfs,
-//     ) -> Result<Vec<ConversationDocument>, Error> {
-//         debug!("Loading conversations");
-//         let list = FuturesUnordered::from_iter(
-//             self.conversations
-//                 .iter()
-//                 .map(|document| async { document.resolve(ipfs.clone(), None).await }),
-//         )
-//         .filter_map(|res| async { res.ok() })
-//         .collect::<Vec<_>>()
-//         .await;
-//         info!("Conversations loaded");
-//         Ok(list)
-//     }
-
-//     pub async fn remove_conversation(
-//         &mut self,
-//         ipfs: Ipfs,
-//         conversation_id: Uuid,
-//     ) -> Result<ConversationDocument, Error> {
-//         info!("Removing conversation");
-//         let document_type = self
-//             .get_conversation_document(ipfs.clone(), conversation_id)
-//             .await?;
-
-//         if !self.conversations.remove(&document_type) {
-//             error!("Conversation doesnt exist");
-//             return Err(Error::InvalidConversation);
-//         }
-
-//         let conversation = document_type.resolve(ipfs.clone(), None).await?;
-//         if ipfs.is_pinned(&document_type.document).await? {
-//             info!("Unpinning document");
-//             ipfs.remove_pin(&document_type.document, false).await?;
-//             info!("Document unpinned");
-//         }
-//         ipfs.remove_block(document_type.document).await?;
-//         info!("Block removed");
-
-//         Ok(conversation)
-//     }
-
-//     pub async fn update_conversation(
-//         &mut self,
-//         ipfs: Ipfs,
-//         conversation_id: Uuid,
-//         document: ConversationDocument,
-//     ) -> Result<(), Error> {
-//         let document_type = self
-//             .get_conversation_document(ipfs.clone(), conversation_id)
-//             .await?;
-
-//         if !self.conversations.remove(&document_type) {
-//             return Err(Error::InvalidConversation);
-//         }
-
-//         let document = document.to_document(ipfs.clone()).await?;
-
-//         self.conversations.insert(document);
-
-//         if ipfs.is_pinned(&document_type.document).await? {
-//             ipfs.remove_pin(&document_type.document, false).await?;
-//         }
-//         ipfs.remove_block(document_type.document).await?;
-
-//         Ok(())
-//     }
-// }
