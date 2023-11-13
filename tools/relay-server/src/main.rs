f6c80108204efa23f26388ced15be2dbbfb2e8d0
Darius <darius.clark@proton.me>
2023-11-13 12:10:24 -0500

    chore: Add Logging

--- tools/relay-server/src/main.rs
+++ tools/relay-server/src/main.rs
@@ -52,6 +52,8 @@ struct Opt {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt::init();
+    
     let opts = Opt::parse();
 
     let path = opts.path;
