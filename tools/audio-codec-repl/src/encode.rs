718309c6840dd823c3fc9323ac53f6524a6c8ca2
stavares843 <saratavares843@gmail.com>
2023-11-16 23:35:38 +0000

    chore(typos): fix typo

--- tools/audio-codec-repl/src/encode.rs
+++ tools/audio-codec-repl/src/encode.rs
@@ -137,7 +137,7 @@ pub fn f32_mp4(
             // there is no redundant coding in this sample
             // padding: 0
             // sample_is_non_sync_sample ... set this to 1?
-            // sample_degredation_priority
+            // sample_degradation_priority
             flags: 0, //(2 << 26) | (2 << 24) | (2 << 22) | (2 << 20),
             track_id: 1,
             // stsd entry 1 is for Opus
