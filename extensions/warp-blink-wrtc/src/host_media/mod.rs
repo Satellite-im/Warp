51f0f4fd26013196f924f73567ea13cd7915f110
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-11-29 10:37:10 -0500

    make loopback configurable via  a feature

--- extensions/warp-blink-wrtc/src/host_media/mod.rs
+++ extensions/warp-blink-wrtc/src/host_media/mod.rs
@@ -1,11 +1,15 @@
 mod audio;
 pub mod loopback_controller;
-mod regular_controller;
+pub mod default_controller;
 mod loopback;
 mod mp4_logger;
 
 pub use audio::utils as audio_utils;
 pub use mp4_logger::Mp4LoggerConfig;
 
+#[cfg(feature = "loopback")]
 pub use loopback_controller as controller;
+#[cfg(not(feature = "loopback"))]
+pub use default_controller as controller;
+
 pub const AUDIO_SOURCE_ID: &str = "audio";
