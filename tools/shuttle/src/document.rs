ba9a4282a086875d0e27b38b5f928ef9632b7760
Darius <darius.clark@proton.me>
2023-11-30 11:38:16 -0500

    chore: Remove used structs

--- tools/shuttle/src/document.rs
+++ tools/shuttle/src/document.rs
@@ -1,13 +1,1 @@
-use std::collections::BTreeMap;
-
-use libipld::{Cid, DagCbor};
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, DagCbor)]
-pub struct IdentityRoot {
-    identities: BTreeMap<String, Cid>,
-    package: BTreeMap<String, Cid>,
-}
-
-#[derive(Serialize, Deserialize, DagCbor)]
-pub struct FriendRouterRoot {}
+//TODO: 
