25511883c4261ab7e722251b9dc77cc6771af96b
Darius <darius.clark@proton.me>
2024-02-22 02:55:13 -0500

    chore: Remove check

--- extensions/warp-ipfs/src/store/conversation.rs
+++ extensions/warp-ipfs/src/store/conversation.rs
@@ -947,10 +947,6 @@ impl MessageDocument {
             (!reactions.is_empty()).then_some(ipfs.dag().put().serialize(reactions).await?);
 
         if message.lines() != old_message.lines() {
-            if sender.ne(did) {
-                return Err(Error::InvalidMessage);
-            }
-
             let lines = message.lines();
             if !lines.is_empty() {
                 let lines_value_length: usize = lines
