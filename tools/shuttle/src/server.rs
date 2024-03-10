f4e2e9f21cec1dc9797f7d6cc189ac0a5689d6a8
Darius <darius.clark@proton.me>
2024-03-10 18:36:08 -0400

    chore: change relay configuration

--- tools/shuttle/src/server.rs
+++ tools/shuttle/src/server.rs
@@ -119,33 +119,15 @@ impl ShuttleServer {
 
         if enable_relay_server {
             uninitialized = uninitialized.with_relay_server(RelayConfig {
-                max_circuits: 8198,
-                max_circuits_per_peer: 8198,
-                max_circuit_duration: Duration::from_secs(2 * 60),
-                max_circuit_bytes: 8 * 1024 * 1024,
-                circuit_src_rate_limiters: vec![
-                    RateLimit::PerIp {
-                        limit: 256.try_into().expect("Greater than 0"),
-                        interval: Duration::from_secs(60 * 2),
-                    },
-                    RateLimit::PerPeer {
-                        limit: 256.try_into().expect("Greater than 0"),
-                        interval: Duration::from_secs(60),
-                    },
-                ],
-                max_reservations_per_peer: 512,
-                max_reservations: 8198,
-                reservation_duration: Duration::from_secs(60 * 60),
-                reservation_rate_limiters: vec![
-                    RateLimit::PerIp {
-                        limit: 256.try_into().expect("Greater than 0"),
-                        interval: Duration::from_secs(60),
-                    },
-                    RateLimit::PerPeer {
-                        limit: 256.try_into().expect("Greater than 0"),
-                        interval: Duration::from_secs(60),
-                    },
-                ],
+                max_circuits: usize::MAX,
+                max_circuits_per_peer: usize::MAX,
+                max_circuit_duration: Duration::MAX,
+                max_circuit_bytes: u64::MAX,
+                circuit_src_rate_limiters: vec![],
+                max_reservations_per_peer: usize::MAX / 2,
+                max_reservations: usize::MAX / 2,
+                reservation_duration: Duration::MAX,
+                reservation_rate_limiters: vec![],
             });
         }
 
