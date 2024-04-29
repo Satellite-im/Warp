08f67e533abce8326a4c244c1365517423869b17
Darius <darius.clark@proton.me>
2024-04-28 20:10:03 -0400

    chore: format code

--- extensions/warp-ipfs/src/store/document/image_dag.rs
+++ extensions/warp-ipfs/src/store/document/image_dag.rs
@@ -97,7 +97,7 @@ pub async fn get_image(
         .cat(dag.link)
         // we apply the limit in the event the stream of bytes exceeds the explicit limit
         // which may be the define sized if `limit` is `Some(_)` or no greater
-        // than ImageDag::size, which by usage a limit is usually set as we set a hard limit for 
+        // than ImageDag::size, which by usage a limit is usually set as we set a hard limit for
         // profile images and banners to 2MB
         .max_length(size)
         .providers(peers)
