6b30221fdb02b0dad290edcc09e4a4c6d42a32a8
Darius <darius.clark@proton.me>
2024-06-19 12:48:12 -0400

    chore: Add flag to enable webrtc transport

--- tools/relay-server/src/main.rs
+++ tools/relay-server/src/main.rs
@@ -150,10 +150,14 @@ struct Opt {
     /// Path to a configuration file to adjust relay setting
     #[clap(long)]
     relay_config: Option<PathBuf>,
-    
+
     /// Enables secured websockets
     #[clap(long)]
     enable_wss: bool,
+
+    /// Enables webrtc
+    #[clap(long)]
+    enable_webrtc: bool,
 
     /// Use unbounded configuration with higher limits
     #[clap(long)]
@@ -272,7 +276,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .set_keypair(&keypair)
         .set_idle_connection_timeout(30)
         .set_transport_configuration(TransportConfig {
-            enable_webrtc: true,
+            enable_webrtc: opts.enable_webrtc,
             enable_websocket: true,
             enable_secure_websocket: opts.enable_wss,
             websocket_pem: (ws_cert.is_some() && ws_pk.is_some()).then(|| {
