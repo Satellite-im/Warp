9466647f40a4aa3ebdbc627649db236e2d04ac33
Darius Clark <dariusc93@users.noreply.github.com>
2024-08-15 12:56:50 -0400

    feat: Add description to conversations (#580)

--- extensions/warp-ipfs/examples/messenger.rs
+++ extensions/warp-ipfs/examples/messenger.rs
@@ -1163,6 +1163,20 @@ async fn message_event_handle(
                 writeln!(stdout, ">>> Conversation settings updated: {settings}")?;
             }
         }
+        MessageEventKind::ConversationDescriptionChanged {
+            conversation_id,
+            description,
+        } => {
+            if main_conversation_id == conversation_id {
+                match description {
+                    Some(desc) => writeln!(
+                        stdout,
+                        ">>> Conversation description changed to: \"{desc}\""
+                    )?,
+                    None => writeln!(stdout, ">>> Conversation description removed")?,
+                }
+            }
+        }
     }
 
     Ok(())
