c00d419dbbd445054bfcb6ecaf58779ec0bfe383
ashneverdawn <8341280+ashneverdawn@users.noreply.github.com>
2024-09-05 14:23:02 -0400

    add back config fns, but without wasm_bindgen

--- extensions/warp-ipfs/src/config.rs
+++ extensions/warp-ipfs/src/config.rs
@@ -283,6 +283,40 @@ impl Config {
     }
 }
 
+impl Config {
+    pub fn with_path(&mut self, path: String) {
+        self.path = Some(PathBuf::from(&path))
+    }
+
+    pub fn set_persistence(&mut self, persist: bool) {
+        self.persist = persist
+    }
+
+    pub fn set_relay_enabled(&mut self, enable: bool) {
+        self.enable_relay = enable
+    }
+
+    pub fn set_save_phrase(&mut self, save: bool) {
+        self.save_phrase = save
+    }
+
+    pub fn set_max_storage_size(&mut self, size: Option<usize>) {
+        self.max_storage_size = size
+    }
+
+    pub fn set_max_file_size(&mut self, size: Option<usize>) {
+        self.max_file_size = size
+    }
+
+    pub fn set_thumbnail_size(&mut self, size_x: u32, size_y: u32) {
+        self.thumbnail_size = (size_x, size_y)
+    }
+
+    pub fn with_thumbnail_exact_format(&mut self, exact: bool) {
+        self.thumbnail_exact_format = exact
+    }
+}
+
 impl Default for Config {
     fn default() -> Self {
         #[cfg(not(target_arch = "wasm32"))]
