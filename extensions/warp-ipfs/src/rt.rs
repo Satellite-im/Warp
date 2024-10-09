a9cfe5780a07a7603280a3b2a52e1c6d3b2d98a6
Darius <darius.clark@proton.me>
2024-10-09 11:46:14 -0400

    chore: add abortable handle

--- extensions/warp-ipfs/src/rt.rs
+++ extensions/warp-ipfs/src/rt.rs
@@ -4,25 +4,47 @@ use std::pin::Pin;
 #[cfg(target_arch = "wasm32")]
 use std::task::{Context, Poll};
 
+#[cfg(target_arch = "wasm32")]
+use futures::future::{AbortHandle, Abortable, Aborted};
+
 #[cfg(not(target_arch = "wasm32"))]
 pub use tokio::task::JoinHandle;
 
 #[cfg(target_arch = "wasm32")]
-pub struct JoinHandle<T>(Option<futures::channel::oneshot::Receiver<T>>);
+pub struct JoinHandle<T> {
+    inner: Option<Abortable<futures::channel::oneshot::Receiver<T>>>,
+    handle: AbortHandle,
+}
+
+#[cfg(target_arch = "wasm32")]
+impl<T> JoinHandle<T> {
+    pub fn abort(&mut self) {
+        self.handle.abort();
+        self.inner.take();
+    }
+
+    pub fn is_finished(&self) -> bool {
+        self.handle.is_aborted() || self.inner.is_none()
+    }
+}
 
 #[cfg(target_arch = "wasm32")]
 impl<T> Future for JoinHandle<T> {
     type Output = std::io::Result<T>;
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let Some(this) = self.0.as_mut() else {
+        let Some(this) = self.inner.as_mut() else {
             unreachable!("cannot poll completed future");
         };
 
         let fut = futures::ready!(Pin::new(this).poll(cx));
-        self.0.take();
+        self.inner.take();
 
         match fut {
-            Ok(val) => Poll::Ready(Ok(val)),
+            Ok(Ok(val)) => Poll::Ready(Ok(val)),
+            Ok(Err(e)) => {
+                let e = std::io::Error::other(e);
+                Poll::Ready(Err(e))
+            }
             Err(e) => {
                 let e = std::io::Error::other(e);
                 Poll::Ready(Err(e))
@@ -64,13 +86,20 @@ impl Executor for LocalExecutor {
         }
         #[cfg(target_arch = "wasm32")]
         {
+            let (abort_handle, abort_registration) = AbortHandle::new_pair();
             let (tx, rx) = futures::channel::oneshot::channel();
             let fut = async {
                 let val = future.await;
                 _ = tx.send(val);
             };
-            wasm_bindgen_futures::spawn_local(fut);
-            JoinHandle(Some(rx))
+
+            let abortable_task = Abortable::new(fut, abort_registration);
+
+            wasm_bindgen_futures::spawn_local(abortable_task);
+            JoinHandle {
+                inner: Some(rx),
+                handle: abort_handle,
+            }
         }
     }
 }
