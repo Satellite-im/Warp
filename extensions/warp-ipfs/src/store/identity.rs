ddd0d5a44b3104b9d0ac20880549fd0e271b708c
Darius <darius.clark@proton.me>
2024-12-08 00:43:32 -0500

    chore: remove conversion

--- extensions/warp-ipfs/src/store/identity.rs
+++ extensions/warp-ipfs/src/store/identity.rs
@@ -2001,11 +2001,7 @@ impl IdentityStore {
 
                 match payload.message() {
                     Response::MailboxResponse(MailboxResponse::Receive { list, .. }) => {
-                        let list = list
-                            .iter()
-                            .cloned()
-                            .filter_map(|r| RequestResponsePayload::try_from(r).ok())
-                            .collect::<Vec<_>>();
+                        let list = list.iter().cloned().collect::<Vec<_>>();
 
                         for req in list {
                             let from = req.sender.clone();
