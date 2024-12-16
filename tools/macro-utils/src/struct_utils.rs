9db9e7e0723651bac3f88e6ab7b4bfed9d1e71ba
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-12-16 19:41:58 +0100

    add trait parsing

--- tools/macro-utils/src/struct_utils.rs
+++ tools/macro-utils/src/struct_utils.rs
@@ -1,28 +1,19 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::{
-    parse::{Parse, Parser},
-    punctuated::Punctuated,
-    spanned::Spanned,
-    Error, ImplItem, ItemImpl, Meta, Result, Type, Visibility,
+    parse::{Parse, Parser}, parse_quote, punctuated::Punctuated, spanned::Spanned, Error, ImplItem, ItemImpl, ItemTrait, Meta, Result, TraitItem, Visibility
 };
 
 pub fn expand(attr: TokenStream, input: TokenStream) -> Result<TokenStream> {
     let attributes = Punctuated::<Meta, syn::Token![,]>::parse_terminated.parse(attr)?;
-    let impls: ItemImpl = ItemImpl::parse.parse(input)?;
-    let ident;
-    let mut _name = "functions".into();
-    if let Type::Path(p) = impls.self_ty.as_ref() {
-        ident = p.path.get_ident().unwrap();
-    } else {
-        return Err(Error::new(impls.span(), "Only applicable to impl blocks"));
-    }
+    let mut name = "functions".into();
+    let expanded;
     for attr in attributes {
         if attr.path().is_ident("name") {
             if let syn::Meta::NameValue(val) = &attr {
                 if let syn::Expr::Lit(v) = &val.value {
                     if let syn::Lit::Str(s) = &v.lit {
-                        _name = s.value();
+                        name = s.value();
                     }
                 }
             }
@@ -40,17 +31,49 @@ pub fn expand(attr: TokenStream, input: TokenStream) -> Result<TokenStream> {
         }
     }
 
-    let mut functions = vec![];
-    for item in impls.items {
-        if let ImplItem::Fn(function) = item {
-            if (function
-                .attrs
-                .iter()
-                .find(|attr| attr.path().is_ident("skip")))
-            .is_none()
-            {
-                let is_async = function.sig.asyncness.is_some();
-                if matches!(function.vis, Visibility::Public(_) | Visibility::Inherited) {
+    if let Ok(mut impls) = ItemImpl::parse.parse(input.clone()) {
+        // Parse impl block
+        let mut functions = vec![];
+        for item in impls.items.iter() {
+            if let ImplItem::Fn(function) = item {
+                if (function
+                    .attrs
+                    .iter()
+                    .find(|attr| attr.path().is_ident("skip")))
+                .is_none()
+                {
+                    let is_async = function.sig.asyncness.is_some();
+                    if matches!(function.vis, Visibility::Public(_) | Visibility::Inherited) {
+                        let id = function.sig.ident.to_string();
+                        functions.push(quote! {
+                            (#id, #is_async)
+                        });
+                    }
+                }
+            }
+        }
+        let func = format_ident!("{name}");
+        impls.items.push(parse_quote! {
+            fn #func() -> &'static[(&'static str, bool)] {
+                &[ #(#functions),* ]
+            }
+        });
+        expanded = quote! {
+            #impls
+        };
+    } else {
+        // Parse trait block
+        let mut trait_impl: ItemTrait = ItemTrait::parse.parse(input)?;
+        let mut functions = vec![];
+        for item in &trait_impl.items {
+            if let TraitItem::Fn(function) = item {
+                if (function
+                    .attrs
+                    .iter()
+                    .find(|attr| attr.path().is_ident("skip")))
+                .is_none()
+                {
+                    let is_async = function.sig.asyncness.is_some();
                     let id = function.sig.ident.to_string();
                     functions.push(quote! {
                         (#id, #is_async)
@@ -58,14 +81,15 @@ pub fn expand(attr: TokenStream, input: TokenStream) -> Result<TokenStream> {
                 }
             }
         }
-    }
-    let func = format_ident!("{_name}");
-    let expanded = quote! {
-        impl #ident {
-            pub fn #func() -> &'static[(&'static str, bool)] {
+        let func = format_ident!("{name}");
+        trait_impl.items.push(parse_quote! {
+            fn #func() -> &'static[(&'static str, bool)] {
                 &[ #(#functions),* ]
             }
-        }
-    };
+        });
+        expanded = quote! {
+            #trait_impl
+        };
+    }
     Ok(TokenStream::from(expanded))
 }
