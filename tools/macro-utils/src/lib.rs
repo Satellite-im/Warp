3d5cbd3687b9591a6e037d0232626013f6e81e94
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-12-16 19:56:07 +0100

    update docs

--- tools/macro-utils/src/lib.rs
+++ tools/macro-utils/src/lib.rs
@@ -19,9 +19,9 @@ mod struct_utils;
 ///
 /// assert_eq!([SomeEnum::SomeValue1, SomeEnum::SomeValue2], SomeEnum::values());
 /// 
-/// #[derive(VariantExport, PartialEq, Debug)]
 /// pub struct B;
 /// 
+/// #[derive(VariantExport)]
 /// pub struct A {
 ///     x: i64,
 ///     y: i128,
@@ -37,7 +37,7 @@ pub fn field_values(input: TokenStream) -> TokenStream {
         .into()
 }
 
-/// Implements a way to fetch all public implemented method for a given struct.
+/// Implements a way to fetch all public implemented method for a given struct or trait.
 /// It returns a tuple where the first element is the functions name and the second if its async
 /// E.g.
 /// ```
@@ -45,6 +45,7 @@ pub fn field_values(input: TokenStream) -> TokenStream {
 /// 
 /// pub struct A;
 /// 
+/// /// For structs
 /// #[impl_funcs(name="get_all_impls")] /// Optional name parameter. If not present will be called functions
 /// impl A {
 ///     fn some_method_a() {}
@@ -54,6 +55,28 @@ pub fn field_values(input: TokenStream) -> TokenStream {
 ///     async fn some_async_method_c() {}
 /// }
 /// assert_eq!([("some_method_a", false), ("some_method_b", false), ("some_async_method_c", true)], A::get_all_impls());
+/// 
+/// 
+/// /// For traits
+/// #[impl_funcs(name="get_all_impls")] /// Optional name parameter. If not present will be called functions
+/// trait SomeTrait {
+///     fn some_method_a();
+/// 
+///     fn some_method_b();
+/// 
+///     async fn some_async_method_c();
+/// }
+/// 
+/// pub struct B;
+/// impl SomeTrait for B {
+///     fn some_method_a() {}
+/// 
+///     fn some_method_b() {}
+/// 
+///     async fn some_async_method_c() {}
+/// }
+/// 
+/// assert_eq!([("some_method_a", false), ("some_method_b", false), ("some_async_method_c", true)], B::get_all_impls());
 /// ```
 #[proc_macro_attribute]
 pub fn impl_funcs(attr: TokenStream, input: TokenStream) -> TokenStream {
