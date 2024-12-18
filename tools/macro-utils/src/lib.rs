9f8e7e247fc467d772f05ef169ee1a20f1dddb7e
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-12-18 16:31:55 +0100

    remove field expose macro

--- tools/macro-utils/src/lib.rs
+++ tools/macro-utils/src/lib.rs
@@ -2,40 +2,7 @@ extern crate proc_macro2;
 
 use proc_macro::TokenStream;
 
-mod field_utils;
 mod struct_utils;
-
-/// Implements either:
-///     #values() -> &'static[#Enum] for enums that returns all defined values in the enum.
-///     #fields() -> &'static[(field_name, field_type_definition)] for struct and union types that returns all defined fields.
-/// E.g.
-/// ```
-/// use macro_utils::VariantExport;
-/// #[derive(VariantExport, PartialEq, Debug)]
-/// pub enum SomeEnum {
-///     SomeValue1,
-///     SomeValue2
-/// }
-///
-/// assert_eq!([SomeEnum::SomeValue1, SomeEnum::SomeValue2], SomeEnum::values());
-/// 
-/// pub struct B;
-/// 
-/// #[derive(VariantExport)]
-/// pub struct A {
-///     x: i64,
-///     y: i128,
-///     z: B
-/// }
-///
-/// assert_eq!([("x", "i64"), ("y", "i128"), ("z", "B")], A::fields());
-/// ```
-#[proc_macro_derive(VariantExport)]
-pub fn field_values(input: TokenStream) -> TokenStream {
-    field_utils::expand(input)
-        .unwrap_or_else(|e| e.into_compile_error().into())
-        .into()
-}
 
 /// Implements a way to fetch all public implemented method for a given struct or trait.
 /// It returns a tuple where the first element is the functions name and the second if its async
